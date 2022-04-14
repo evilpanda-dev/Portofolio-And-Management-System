6b87c069aeaa2600c6956d5f63e647834d0a382e
EvilPanda <joratractoristul@gmail.com>
2022-04-14 19:41:51 +0300

    Inherit IUser

--- CVapp/Models/Authentification/User.cs
+++ CVapp/Models/Authentification/User.cs
@@ -1,9 +1,10 @@
-﻿using CVapp.Repository;
+﻿using CVapp.Repository.GenericRepository;
+using CVapp.Repository.UserRepository;
 using System.Text.Json.Serialization;
 
 namespace CVapp.Models.Authentification
 {
-    public class User : IEntityBase
+    public class User : IEntityBase,IUser
     {
         public int Id { get; set; }
         public string UserName { get; set; }
