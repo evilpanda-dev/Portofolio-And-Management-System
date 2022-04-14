733d853a4b9715204886ecaec76c346b5c41a6b4
EvilPanda <joratractoristul@gmail.com>
2022-04-14 03:51:51 +0300

    Add fluent api configuration for User table

---
+++ CVapp/Models/Configurations/UserConfig.cs
@@ -0,0 +1,30 @@
+﻿using CVapp.Models.Authentification;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CVapp.Models.Configurations
+{
+    public class UserConfig : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasKey(u => u.UserId);
+            builder.Property(u => u.UserId).ValueGeneratedOnAdd()
+                .HasColumnName("UserId")
+                .HasColumnType("int");
+            builder.Property(u => u.UserName)
+                .IsRequired()
+                .HasMaxLength(40)
+                .HasColumnType("nvarchar");
+            builder.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(40)
+                .HasColumnType("nvarchar");
+            builder.Property(u => u.Password)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnType("nvarchar");
+        }
+    }
+}
