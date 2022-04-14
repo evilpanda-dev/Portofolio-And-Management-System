025f6bebf9fc8ecfd874f1e970331147c757c249
EvilPanda <joratractoristul@gmail.com>
2022-04-14 03:24:43 +0300

    Create the controller for the authentification

---
+++ CVapp/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CVapp.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return Ok("succes");
+        }
+    }
+}
