3df5e1e1ea8f0e536e0b195b637a25ac7132cb15
EvilPanda <joratractoristul@gmail.com>
2022-04-14 05:59:02 +0300

    Create the post request for registering

--- CVapp/Controllers/AuthController.cs
+++ CVapp/Controllers/AuthController.cs
@@ -1,15 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CVapp.DTOs;
+using CVapp.Models.Authentification;
+using CVapp.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CVapp.Controllers
 {
-    [Route("")]
     [ApiController]
+    [Route("api")]
     public class AuthController : Controller
     {
-        [HttpGet]
-        public IActionResult Register()
+        private readonly IRepository<User> _repository;
+        public AuthController(IRepository<User> repository)
         {
-            return Ok("succes");
+            _repository = repository;
+        }
+            
+        [HttpPost("register")]
+        public IActionResult Register(RegisterDto dto)
+        {
+            var user = new User
+            {
+                UserName = dto.UserName,
+                Email = dto.Email,
+                Password = BCrypt.Net.BCrypt.HashPassword(dto.Password)
+            };
+            
+            return Created("succes", _repository.Create(user));
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginDto dto)
+        {
+            
         }
     }
 }
