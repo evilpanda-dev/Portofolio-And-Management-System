aab8b8269a563a379dfabe5aa70119931f40528a
EvilPanda <joratractoristul@gmail.com>
2022-04-14 05:57:32 +0300

    Inherit from IEntityBase and change the Id spelling

--- CVapp/Models/Authentification/User.cs
+++ CVapp/Models/Authentification/User.cs
@@ -1,8 +1,10 @@
-﻿namespace CVapp.Models.Authentification
+﻿using CVapp.Repository;
+
+namespace CVapp.Models.Authentification
 {
-    public class User
+    public class User : IEntityBase
     {
-        public int UserId { get; set; }
+        public int Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
