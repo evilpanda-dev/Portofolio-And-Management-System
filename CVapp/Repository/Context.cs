6f4af1286d41ce3495309059133a8aa0800a9176
EvilPanda <joratractoristul@gmail.com>
2022-04-14 03:23:38 +0300

    Create the context of the app

---
+++ CVapp/Repository/Context.cs
@@ -0,0 +1,38 @@
+﻿using CVapp.Models.Authentification;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace CVapp.Repository
+{
+    public class Context : DbContext
+    {
+        private readonly string _connectionString;
+        public DbSet<User> Users { get; set; }
+        
+        public Context()
+        { 
+        }
+
+        public Context(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetAssembly(typeof(Context)));
+        }
+    }
+}
