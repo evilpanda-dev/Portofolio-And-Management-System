1d7cdb156ce5abde42c99ff8e654ec9cebb681a7
EvilPanda <joratractoristul@gmail.com>
2022-04-14 07:23:16 +0300

    Create all the apis for log in/out and for getting the user

--- CVapp/Controllers/AuthController.cs
+++ CVapp/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using CVapp.DTOs;
+using CVapp.Helpers;
 using CVapp.Models.Authentification;
 using CVapp.Repository;
 using Microsoft.AspNetCore.Mvc;
@@ -10,11 +11,13 @@ namespace CVapp.Controllers
     public class AuthController : Controller
     {
         private readonly IRepository<User> _repository;
-        public AuthController(IRepository<User> repository)
+        private readonly JwtService _jwtService;
+        public AuthController(IRepository<User> repository, JwtService jwtService)
         {
             _repository = repository;
+            _jwtService = jwtService;
         }
-            
+
         [HttpPost("register")]
         public IActionResult Register(RegisterDto dto)
         {
@@ -24,14 +27,58 @@ namespace CVapp.Controllers
                 Email = dto.Email,
                 Password = BCrypt.Net.BCrypt.HashPassword(dto.Password)
             };
-            
+
             return Created("succes", _repository.Create(user));
         }
 
         [HttpPost("login")]
         public IActionResult Login(LoginDto dto)
         {
-            
+            var user = _repository.GetByEmail(dto.Email);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Invalid Credentials" });
+            }
+            if (!BCrypt.Net.BCrypt.Verify(dto.Password, user.Password))
+            {
+                return BadRequest(new { message = "Invalid Credentials" });
+            }
+            var jwt = _jwtService.Generate(user.Id);
+
+            Response.Cookies.Append("jwt", jwt, new CookieOptions
+            {
+                HttpOnly = true,
+            });
+
+            return Ok(new { message = "Succesfully logged in" });
+        }
+
+        [HttpGet("user")]
+        public IActionResult User()
+        {
+            try
+            {
+                var jwt = Request.Cookies["jwt"];
+
+                var token = _jwtService.Verify(jwt);
+
+                int userId = int.Parse(token.Issuer);
+
+                var user = _repository.GetById(userId);
+
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                return Unauthorized();
+            }
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("jwt");
+            return Ok(new { message = "Succesfully logged out" });
         }
     }
 }
