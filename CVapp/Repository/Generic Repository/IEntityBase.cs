c7d80d828cb1f3ed1798d47e53b291cfa83f1045
EvilPanda <joratractoristul@gmail.com>
2022-04-14 19:41:34 +0300

    Move the generic repository in its folder

---
+++ CVapp/Repository/Generic Repository/IEntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace CVapp.Repository.GenericRepository
+{
+    public interface IEntityBase
+    {
+        public int Id { get; set; }
+        //string Email { get; set; }
+    }
+}
---
+++ CVapp/Repository/Generic Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace CVapp.Repository.GenericRepository
+{
+    public interface IRepository<TEntity> where TEntity : IEntityBase
+    {
+        TEntity Create(TEntity entity);
+        void Delete(TEntity entity);
+        void Delete(int id);
+        TEntity Update(TEntity entity);
+
+        //read data
+        TEntity GetById(int id);
+        //public TEntity GetByEmail(string email);
+        IEnumerable<TEntity> Filter();
+        IEnumerable<TEntity> Filter(Func<TEntity, bool> predicate);
+
+        void SaveChanges();
+    }
+}
