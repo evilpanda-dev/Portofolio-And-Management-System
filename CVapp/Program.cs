cdd0f0db39ca81e2a87f81cc75cc0e4ddfcac034
EvilPanda <joratractoristul@gmail.com>
2022-04-15 19:30:51 +0300

    Add jwt authentication and constant for jwt in appsetttings.json

--- CVapp/Program.cs
+++ CVapp/Program.cs
@@ -2,10 +2,15 @@ using CVapp.Helpers;
 using CVapp.Repository;
 using CVapp.Repository.GenericRepository;
 using CVapp.Repository.UserRepository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//var secureKey = "this is my super secure key";
+//var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secureKey));
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -14,9 +19,25 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
 builder.Services.AddScoped<DbContext, Context>();
-builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped(typeof(IUserRepository<>), typeof(UserRepository<>));
 builder.Services.AddScoped<JwtService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => 
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+           // ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Token"]))
+        };
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,11 +49,13 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseCors(options => options
-.WithOrigins(new[] { "http://localhost:3000","http://localhost:8080","http://localhost:4200"})
+.WithOrigins(new[] { "http://localhost:3000", "http://localhost:8080", "http://localhost:4200" })
 .AllowCredentials()
     .AllowAnyMethod()
     .AllowAnyHeader());
 
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
