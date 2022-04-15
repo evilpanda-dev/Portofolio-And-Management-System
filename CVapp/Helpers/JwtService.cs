39dc0ed1cfec0ebeab847839f87e1e424fa19009
EvilPanda <joratractoristul@gmail.com>
2022-04-15 19:29:30 +0300

    Add constant for audience

--- CVapp/Helpers/JwtService.cs
+++ CVapp/Helpers/JwtService.cs
@@ -1,19 +1,31 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using CVapp.DTOs;
+using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace CVapp.Helpers
 {
     public class JwtService
     {
-        private string secureKey = "this is my super secure key";
-        public string Generate(int id)
+        //private string secureKey = "this is my super secure key";
+        private readonly IConfiguration _config;
+        public JwtService(IConfiguration config)
         {
-            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secureKey));
+            _config = config;
+        }
+
+        public string Generate(LoginDto loginUser,int id)
+        {
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Token"]));
             var credentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
             var header = new JwtHeader(credentials);
+            var claims = new[]
+            {
+               new Claim("email", loginUser.Email),
+            };
 
-            var payload = new JwtPayload(id.ToString(), null, null, null, DateTime.Today.AddDays(1)); //Expire in one day
+            var payload = new JwtPayload(id.ToString(), _config["Jwt:Audience"], claims, null, DateTime.Today.AddDays(1)); ; //Expire in one day
 
             var securityToken = new JwtSecurityToken(header, payload);
             return new JwtSecurityTokenHandler().WriteToken(securityToken);
@@ -21,8 +33,8 @@ namespace CVapp.Helpers
 
         public JwtSecurityToken Verify(string jwt)
         {
-            var tokenHandler =new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(secureKey);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config["Jwt:Token"]);
             tokenHandler.ValidateToken(jwt, new TokenValidationParameters
             {
                 ValidateIssuerSigningKey = true,
