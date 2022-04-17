1624758f58f62ea5685cff92e8782d862eed15d8
EvilPanda <joratractoristul@gmail.com>
2022-04-17 18:24:05 +0300

    Create registerButton reducer

---
+++ src/store/reducers/registerButtonReducer.js
@@ -0,0 +1,17 @@
+const defaultState = {
+    popup: false
+  }
+  
+  const REGISTER_CLICKED = 'REGISTER_CLICKED'
+  const REGISTER_CLOSED = 'REGISTER_CLOSED'
+  
+  export const registerButtonReducer = (state = defaultState, action) => {
+    switch (action.type) {
+      case REGISTER_CLICKED:
+        return { ...state, popup: action.payload }
+      case REGISTER_CLOSED:
+        return { ...state, popup: action.payload }
+      default:
+        return state
+    }
+  }
