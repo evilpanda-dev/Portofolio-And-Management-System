59cb1d72c9e8deb4af7b97039dc2456816209cc4
EvilPanda <joratractoristul@gmail.com>
2022-04-18 01:54:21 +0300

    Add conditional rendering in header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,33 @@
 import LoginForm from "../LoginForm/LoginForm";
 import RegistrationForm from "../RegistrationForm/RegistrationForm";
+import Logout from "../Logout/Logout";
 import "./Header.css";
-const Header = () =>{
+
+const Header = (props) =>{
+  const {
+    userName,
+    setUserName
+  } = props
+
+let menu ;
+if (userName==="") {
+  menu = (
+    <>
+    <a><LoginForm setUserName={setUserName}/></a>
+    <a ><RegistrationForm/></a>
+    </>
+  )
+} else {
+  menu = (
+<a><Logout setUserName={setUserName}/></a>
+  )
+}
+
     return (
         <div class="header">
         <div class="header-right">
           <a class="active" href="/">Home</a>
-          <a><LoginForm/></a>
-          <a ><RegistrationForm/></a>
+         {menu}
         </div>
       </div>
     )
