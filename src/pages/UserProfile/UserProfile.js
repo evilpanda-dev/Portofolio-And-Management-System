4191cdf0464e7ddd36882e89968ded844099d6d7
EvilPanda <joratractoristul@gmail.com>
2022-04-18 17:02:30 +0300

    Create user profile page

---
+++ src/pages/UserProfile/UserProfile.js
@@ -0,0 +1,17 @@
+import Header from '../../components/Header/Header';
+import Panel from '../../components/Panel/Panel';
+import './UserProfile.css'
+
+const UserProfile = () =>{
+    return (
+        <>
+<Panel />
+<Header/>
+<section className='profilePage'>
+
+</section>
+</>
+    )
+}
+
+export default UserProfile;
