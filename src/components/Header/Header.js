0ffe6f3e257ffcb22cc57665be135f0f3e297abb
EvilPanda <joratractoristul@gmail.com>
2022-04-18 17:02:50 +0300

    Add user avatar on the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import LoginForm from "../LoginForm/LoginForm";
 import RegistrationForm from "../RegistrationForm/RegistrationForm";
 import Logout from "../Logout/Logout";
+import UserProfile from "../../pages/UserProfile/UserProfile";
 import "./Header.css";
 
 const Header = (props) =>{
@@ -19,14 +20,17 @@ if (userName==="") {
   )
 } else {
   menu = (
-<a><Logout setUserName={setUserName}/></a>
+    <>
+  <a href="/profile" className="profileLink"> <img src="http://avatars0.githubusercontent.com/u/246180?v=4" className="profileAvatar"/></a>
+    <a><Logout setUserName={setUserName}/></a>
+    </>
   )
 }
 
     return (
         <div className="header">
         <div className="header-right">
-          <a className="active" href="/">Home</a>
+          <a className="active" href="/Inner">Home</a>
          {menu}
         </div>
       </div>
