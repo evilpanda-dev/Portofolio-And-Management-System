d5239c1870f7a5e5d669d075912e19d34e755514
EvilPanda <joratractoristul@gmail.com>
2022-04-20 01:31:29 +0300

    Create user profile repository

---
+++ CVapp/Repository/UserProfileRepository/UserProfileRepository.cs
@@ -0,0 +1,25 @@
+﻿using CVapp.Models.Authentificated;
+using CVapp.Repository.GenericRepository;
+using CVapp.Repository.UserProfileRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CVapp.Repository.UserRepository
+{
+    public class UserProfileRepository<TEntity> : IUserProfileRepository<TEntity> where TEntity : class, IUserProfile
+    {
+        private readonly DbContext _context;
+        private DbSet<TEntity> _dbSet;
+        
+        public UserProfileRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public TEntity GetByUserId(int id)
+        {
+            return _dbSet.FirstOrDefault(e => e.UserId == id);
+        }
+    }
+}
