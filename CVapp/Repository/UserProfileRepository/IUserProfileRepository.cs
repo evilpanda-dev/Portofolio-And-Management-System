36a360dcdd66a941a933833c50ddc316843befbb
EvilPanda <joratractoristul@gmail.com>
2022-04-20 01:31:18 +0300

    Create the interface repository for the user profile model

---
+++ CVapp/Repository/UserProfileRepository/IUserProfileRepository.cs
@@ -0,0 +1,10 @@
+﻿using CVapp.Models.Authentificated;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVapp.Repository.UserProfileRepository
+{
+    public interface IUserProfileRepository<TEntity> where TEntity : IUserProfile
+    {
+        public TEntity GetByUserId(int id);
+    }
+}
