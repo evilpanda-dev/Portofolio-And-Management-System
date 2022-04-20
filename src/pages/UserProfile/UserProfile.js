435a7141aeb603f030f7397e153ed56298d32601
EvilPanda <joratractoristul@gmail.com>
2022-04-20 06:26:42 +0300

    Add ProfileForm component on the user profile page

--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -1,14 +1,41 @@
 import Header from '../../components/Header/Header';
 import Panel from '../../components/Panel/Panel';
+import ProfileForm from '../../components/ProfileForm/ProfileForm';
+import { useEffect,useState } from 'react';
 import './UserProfile.css'
 
-const UserProfile = () =>{
+const UserProfile = (props) =>{
+    const{
+        setImage,
+        setUserName,
+        setRole,
+        imageSrc
+    } = props
+
+    // const [image,setImage] = useState('')
+    // useEffect(()=>{
+    //     (
+    //       async () => {
+    //         const response = await fetch('https://localhost:5000/api/userProfile',{
+    //           headers: {'Content-Type': 'application/json'},
+    //           credentials:'include',
+    //       })
+    //       .then(response => response.json())
+    //       .then(data=>{
+    //           console.log(data)
+    //             setImage(data.imgByte)
+    //       })
+    //       }
+    //     )();
+    //     })
+
     return (
         <>
 <Panel />
-<Header/>
+<Header imageSrc={imageSrc} setUserName={setUserName} setRole={setRole}/>
 <section className='profilePage'>
-
+{/* <img alt="not fount" width={"250px"} src={`data:image/jpeg;base64,${image}`} /> */}
+<ProfileForm/>
 </section>
 </>
     )
