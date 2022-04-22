0fba87bd76897baaf075a9759c5628b9b56c8423
EvilPanda <joratractoristul@gmail.com>
2022-04-22 07:14:18 +0300

    Create the test db context

---
+++ IntegrationTestsWebApi/TestDbContext/ContextTest.cs
@@ -0,0 +1,41 @@
+﻿using CVapp.Models.Authentificated;
+using CVapp.Models.Authentification;
+using CVapp.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace IntegrationTestsWebApi.TestDbContext
+{
+    public class ContextTest : DbContext
+    {
+        private readonly string _connectionString = "Data Source=.;Initial Catalog=CVappDbTest;Integrated Security=True";
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+
+        public ContextTest()
+        {
+        }
+
+        public ContextTest(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public ContextTest(DbContextOptions<ContextTest> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetAssembly(typeof(Context)));
+        }
+    }
+}
