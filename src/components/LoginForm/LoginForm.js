da3921f4e4b07163b52d1cdcacf76164628e76ef
EvilPanda <joratractoristul@gmail.com>
2022-04-26 19:14:57 +0300

    Format files with prettier

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,126 +1,137 @@
-import React from 'react';
-import { Formik,Form } from 'formik';
-import * as Yup from 'yup'
-import FormikControl from '../FormikControl/FormikControl.js'
-import './LoginForm.css'
-import { useSelector,useDispatch } from 'react-redux';
-import {useState,useEffect} from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import FormikControl from "../FormikControl/FormikControl.js";
+import "./LoginForm.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const validationSchema = Yup.object({
+  email: Yup.string().email("Invalid email format").required("Required"),
+  password: Yup.string()
+    .required("Required")
+    .min(6, "Password must be at least 8 characters"),
+});
+
+const initialValues = {
+  email: "",
+  password: "",
+};
 
 const LoginForm = (props) => {
-  const {
-    setUserName,
-    setRole
-  } = props
+  const { setUserName, setRole } = props;
 
-  const [Email,setEmail] = useState('')
-  const [Password,setPassword] = useState('')
-  const [redirect,setRedirect] = useState(false)
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
-    const initialValues = {
-        email: '',
-        password: ''
-      }
-      
-    
-      const validationSchema = Yup.object({
-        email: Yup.string()
-          .email('Invalid email format')
-          .required('Required'),
-        password: Yup.string().required('Required')
-        .min(6,'Password must be at least 8 characters')
-      })
-    
-      const onSubmit = async() => {
-       const response = await fetch('https://localhost:5000/api/login',{
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        credentials:'include',
-body: JSON.stringify({
-    Email,
-    Password
-})
+  const onSubmit = async () => {
+    const response = await fetch("https://localhost:5000/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify({
+        Email,
+        Password,
+      }),
     });
-const content = await response.json()
-setUserName(content.userName)
-setRole(content.role)
-    if(response.status === 200){
-    setRedirect(true)
+    const content = await response.json();
+    setUserName(content.userName);
+    setRole(content.role);
+    if (response.status === 200) {
+      setRedirect(true);
     }
-  }
-    
-    const isVisible = useSelector(state => state.popupState.popup)
-    const dispatch = useDispatch()
+  };
 
-    const activateSectionVisibility = () => {
-        dispatch({ type: 'LOGIN_CLICKED', payload: true })
+  const isVisible = useSelector((state) => state.popupState.popup);
+  const dispatch = useDispatch();
+
+  const activateSectionVisibility = () => {
+    dispatch({ type: "LOGIN_CLICKED", payload: true });
+  };
+
+  const deactivateSectionVisibility = () => {
+    dispatch({ type: "LOGIN_CLOSED", payload: false });
+  };
+
+  const changeButtonState = () => {
+    isVisible ? deactivateSectionVisibility() : activateSectionVisibility();
+  };
+
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (redirect) {
+      deactivateSectionVisibility();
+      navigate("/Inner");
     }
+  }, [redirect]);
 
-    const deactivateSectionVisibility = () => {
-        dispatch({ type: 'LOGIN_CLOSED', payload: false })
-    }
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {(formik) => {
+        return (
+          <Form>
+            <div className="loginButton">
+              <button
+                id="showLogin"
+                className="showLoginButton"
+                onClick={changeButtonState}
+              >
+                Login
+              </button>
+            </div>
+            <div className={isVisible ? "popup active" : "popup"}>
+              <div className="closeBtn" onClick={changeButtonState}>
+                &times;
+              </div>
+              <div className="form">
+                <h2>Log in</h2>
+                <div className="formElement">
+                  <FormikControl
+                    control="input"
+                    type="email"
+                    label="Email"
+                    name="email"
+                    placeholder="Enter your email"
+                    value={Email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                      formik.handleChange(e);
+                    }}
+                  />
+                </div>
+                <div className="formElement">
+                  <FormikControl
+                    control="input"
+                    type="password"
+                    label="Password"
+                    name="password"
+                    placeholder="Enter your password"
+                    value={Password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                      formik.handleChange(e);
+                    }}
+                  />
+                </div>
+                <div className="formElement">
+                  <button type="submit" disabled={!formik.isValid}>
+                    Sign in
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
 
-    const changeButtonState = () => {
-        isVisible ? deactivateSectionVisibility() : activateSectionVisibility()
-    }
-
-    let navigate = useNavigate();
-
-    useEffect(()=>{
-      if(redirect){
-          deactivateSectionVisibility();
-        return  navigate("/Inner")
-      }
-          },[redirect])
-
-      return (
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={onSubmit}
-        >
-          {formik => {
-            return (
-              <Form>
-                  <div className="loginButton">
-                      <button id="showLogin" className="showLoginButton" onClick={changeButtonState}>Login</button>
-                  </div>
-                  <div className={isVisible ? "popup active" : "popup"}>
-                      <div className='closeBtn' onClick={changeButtonState}>&times;</div>
-                      <div className="form">
-                          <h2>Log in</h2>
-                          <div className='formElement'>
-                          <FormikControl
-                  control='input'
-                  type='email'
-                  label='Email'
-                  name='email'
-                  placeholder='Enter your email'
-                  value={Email}
-                onChange={(e) => {setEmail(e.target.value); formik.handleChange(e)}}
-                />
-                          </div>
-                          <div className='formElement'>
-                          <FormikControl
-                  control='input'
-                  type='password'
-                  label='Password'
-                  name='password'
-                  placeholder='Enter your password'
-                  value={Password}
-                  onChange={(e) => {setPassword(e.target.value);formik.handleChange(e)}}
-                />
-                          </div>
-                          <div className='formElement'>
-                          <button type='submit' disabled={!formik.isValid}>Sign in</button>
-                          </div>
-                      </div>
-                  </div>
-              </Form>
-            )
-          }}
-        </Formik>
-      )
-    }
-    
-    export default LoginForm
+export default LoginForm;
