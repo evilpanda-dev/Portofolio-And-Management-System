9ffc02e1986ec4fe0e0d61ddf0a8e035d34ff338
EvilPanda <joratractoristul@gmail.com>
2022-04-26 16:32:23 +0300

    Create user profile service and add user profile dto

--- CVapp/Controllers/UserProfileController.cs
+++ CVapp/Controllers/UserProfileController.cs
@@ -1,5 +1,6 @@
 ﻿using CVapp.Domain.Models.Authentificated;
 using CVapp.Infrastructure.Abstractions;
+using CVapp.Infrastructure.DTOs;
 using CVapp.Infrastructure.Repository.UserProfileRepository;
 using CVapp.Infrastructure.Services;
 using LoggerService;
@@ -19,75 +20,28 @@ namespace CVapp.API.Controllers
         private readonly IWebHostEnvironment _hostEnvironment;
         private readonly ILoggerManager _logger;
         private readonly JwtService _jwtService;
-       // private readonly IUserProfileService _userProfileService;
+        private readonly IUserProfileService _userProfileService;
         private readonly IUserProfileRepository<UserProfile> _userProfileRepository;
 
-        public UserProfileController(IWebHostEnvironment hostEnvironment,ILoggerManager logger,JwtService jwtService,IUserProfileRepository<UserProfile> userProfileRepository)
+        public UserProfileController(IWebHostEnvironment hostEnvironment,
+                                     ILoggerManager logger,
+                                     JwtService jwtService,
+                                     IUserProfileRepository<UserProfile> userProfileRepository,
+                                     IUserProfileService userProfileService)
         {
             _hostEnvironment = hostEnvironment;
             _logger = logger;
             _jwtService = jwtService;
-          //  _userProfileService = userProfileService;
+            _userProfileService = userProfileService;
             _userProfileRepository = userProfileRepository;
         }
-
+        
         [HttpPost("avatar")]
-        public IActionResult SaveAvatar([FromForm] UserProfile userProfile)
+        public IActionResult SaveAvatar([FromForm] UserProfileDto userProfileDto)
         {
-            try
-            {
-                var jwtCookie = Request.Cookies["jwt"];
-                var token = _jwtService.Verify(jwtCookie);
-                int userId = int.Parse(token.Issuer);
-                var user = _userProfileRepository.GetById(userId);
-                var getId = userId;
-
-                string message = "";
-                var files = userProfile.Files;
-                userProfile.Files = null;
-                userProfile.UserId = getId;
-                userProfile = _userProfileRepository.Create(userProfile);
-
-                if (userProfile.Id > 0 && files != null && files.Length > 0)
-                {
-                    string path = _hostEnvironment.WebRootPath + "\\usersAvatar\\";
-                    if (!Directory.Exists(path))
-                    {
-                        Directory.CreateDirectory(path);
-                    }
-                    string fileName = "avatarPic_" + userProfile.UserId + ".jpg";
-                    if (System.IO.File.Exists(path + fileName))
-                    {
-                        System.IO.File.Delete(path + fileName);
-                    }
-                    using (FileStream fileStream = System.IO.File.Create(path + fileName))
-                    {
-                        files.CopyTo(fileStream);
-                        fileStream.Flush();
-                        message = "Success";
-                    }
-                }
-                else if (userProfile.Id == 0)
-                {
-                    message = "Failed";
-                }
-                else
-                {
-                    message = "Success";
-                }
-                if (message == "Success")
-                {
-                    return Ok(userProfile);
-                }
-                else
-                {
-                    return StatusCode((int)HttpStatusCode.InternalServerError, message);
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
-            }
+                string path = _hostEnvironment.WebRootPath + "\\usersAvatar\\";
+                var userProfile = _userProfileService.SaveAvatar(path, userProfileDto);
+                return Ok(userProfile);
         }
 
 
@@ -105,10 +59,24 @@ namespace CVapp.API.Controllers
                 return Ok(new UserProfile());
             }
             var userProfile = _userProfileRepository.GetById(id);
+            var userProfileDto = new UserProfileDto
+            {
+                AboutMe = userProfile.AboutMe,
+                Address = userProfile.Address,
+                BirthDate = userProfile.BirthDate,
+                City = userProfile.City,
+                Country = userProfile.Country,
+                FirstName = userProfile.FirstName,
+                LastName = userProfile.LastName,
+                Id = userProfile.Id,
+                ModifiedDate = userProfile.ModifiedDate,
+                PhoneNumber = userProfile.PhoneNumber,
+                UserId = userId
+            };
             string fileName = "avatarPic_" + userProfile.UserId + ".jpg";
             var path = Path.Combine(_hostEnvironment.WebRootPath, "usersAvatar", fileName);
-            userProfile.ImgByte = System.IO.File.ReadAllBytes(path);
-            return Ok(userProfile);
+            userProfileDto.ImgByte = System.IO.File.ReadAllBytes(path);
+            return Ok(userProfileDto);
         }
 
     }
