de06bfac351ad201a9daeff5624241ea9a2160c1
EvilPanda <joratractoristul@gmail.com>
2022-04-27 15:53:34 +0300

    Add UserProvider context

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./assets/styles/scss/index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import UserProvider from "./providers/UserProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
+    <UserProvider>
     <App />
+    </UserProvider>
   </Router>
 );
