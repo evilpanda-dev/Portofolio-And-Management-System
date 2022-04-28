6b691460910e1482e23de77d641ca054ab88c1de
EvilPanda <joratractoristul@gmail.com>
2022-04-28 22:50:53 +0300

    Create AvatarNotFoundException

---
+++ CVapp.Infrastructure/Exceptions/AvatarNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.Exceptions
+{
+    public class AvatarNotFoundException : ApplicationException
+    {
+        public AvatarNotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
