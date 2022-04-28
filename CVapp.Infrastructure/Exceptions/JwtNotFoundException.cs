90b8536dc0a6d127c7b4e2d44d7e0198b525e782
EvilPanda <joratractoristul@gmail.com>
2022-04-28 22:51:13 +0300

    Create JwtNotFoundException

---
+++ CVapp.Infrastructure/Exceptions/JwtNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.Exceptions
+{
+    public class JwtNotFoundException : ApplicationException
+    {
+        public JwtNotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
