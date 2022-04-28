c6f78edc06a7c793018ca14ae7d3fcd6f7078914
EvilPanda <joratractoristul@gmail.com>
2022-04-28 22:06:49 +0300

    Create IPropertyMapper with methods for mapping through properties

---
+++ CVapp.Infrastructure/Helpers/IPropertyMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.Helpers
+{
+    public interface IPropertyMapper<T1,T2>
+    {
+        public PropertyMapper<T1, T2> ForMember<T3>(Expression<Func<T1, T3>> funcType1);
+        public PropertyMapper<T1, T2> Map<T1, T2>(T1 source, T2 destination);
+    }
+}
