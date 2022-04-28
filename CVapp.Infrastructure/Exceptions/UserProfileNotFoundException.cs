c9af76bfa8d44b91c1ecad7850920abc7702eaf5
EvilPanda <joratractoristul@gmail.com>
2022-04-28 22:51:25 +0300

    Create UserProfileNotFoundException

---
+++ CVapp.Infrastructure/Exceptions/UserProfileNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.Exceptions
+{
+    public class UserProfileNotFoundException : ApplicationException
+    {
+        public UserProfileNotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
