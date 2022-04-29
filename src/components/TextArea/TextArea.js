a7bd347fb0127349bc0cb44f5deac812de6824e6
EvilPanda <joratractoristul@gmail.com>
2022-04-29 21:20:01 +0300

    Add classnames to text area component

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -3,12 +3,12 @@ import { Field, ErrorMessage } from "formik";
 import TextError from "../TextError/TextError.js";
 
 const Textarea = (props) => {
-  const { label, name, ...rest } = props;
+  const { label, name,labelClass,inputClass,inputError, ...rest } = props;
   return (
     <div className="form-control">
-      <label htmlFor={name}>{label}</label>
-      <Field as="textarea" id={name} name={name} {...rest} />
-      <ErrorMessage component={TextError} name={name} />
+      <label htmlFor={name} className={labelClass}>{label}</label>
+      <Field as="textarea" id={name} name={name} className={inputClass} {...rest} />
+      <ErrorMessage component={TextError} name={name} className={inputError} />
     </div>
   );
 };
