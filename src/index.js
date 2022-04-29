0c98a7a2e8e87b2487273da27dde6710ec7164ce
EvilPanda <joratractoristul@gmail.com>
2022-04-29 18:30:50 +0300

    Add userProfileProvider in index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./assets/styles/scss/index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import UserProvider from "./providers/UserProvider";
+import UserProfileProvider from "./providers/UserProfileProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <UserProvider>
+      <UserProfileProvider>
     <App />
+    </UserProfileProvider>
     </UserProvider>
   </Router>
 );
