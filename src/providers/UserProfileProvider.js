f94aa70ea135682d5d0e04906748413a624dd8b7
EvilPanda <joratractoristul@gmail.com>
2022-04-29 18:30:17 +0300

    Createe UserProfileProvider for storing and managint the User Profile context

---
+++ src/providers/UserProfileProvider.js
@@ -0,0 +1,11 @@
+import { createContext,useState } from "react";
+
+export const UserProfileContext = createContext({})
+
+const UserProfileProvider=({children}) =>{
+const [userProfile,setUserProfile]=useState({})
+
+return <UserProfileContext.Provider value={{userProfile,setUserProfile}}>{children}</UserProfileContext.Provider>
+}
+
+export default UserProfileProvider
