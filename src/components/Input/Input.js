ec22dc96155eda875a3e874b3ae4386658109bdb
EvilPanda <joratractoristul@gmail.com>
2022-04-29 21:19:16 +0300

    Add classnames for input component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,12 +3,12 @@ import { Field, ErrorMessage } from "formik";
 import TextError from "../TextError/TextError.js";
 
 const Input = (props) => {
-  const { label, name, ...rest } = props;
+  const { label, name,labelClass,inputClass,inputError, ...rest } = props;
   return (
     <div className="form-control">
-      <label htmlFor={name}>{label}</label>
-      <Field id={name} name={name} {...rest} />
-      <ErrorMessage component={TextError} name={name} />
+      <label htmlFor={name} className={labelClass}>{label}</label>
+      <Field id={name} name={name} className={inputClass} {...rest} />
+      <ErrorMessage component={TextError} name={name} className={inputError}/>
     </div>
   );
 };
