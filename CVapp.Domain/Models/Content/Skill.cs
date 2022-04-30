81f24837d509b65794cad34f0f8f78733c3b2aa2
EvilPanda <joratractoristul@gmail.com>
2022-04-30 21:11:23 +0300

    Create skill model

---
+++ CVapp.Domain/Models/Content/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Domain.Models.Content
+{
+    public class Skill : IEntityBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Range { get; set; }
+    }
+}
