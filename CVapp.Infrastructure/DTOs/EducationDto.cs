767f3e6dd96eb59a1ed5e228d70adbb763790a7e
EvilPanda <joratractoristul@gmail.com>
2022-04-30 21:13:05 +0300

    Create education dto

---
+++ CVapp.Infrastructure/DTOs/EducationDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.DTOs
+{
+    public class EducationDto
+    {
+        public int Id { get; set; }
+        public int Date { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
