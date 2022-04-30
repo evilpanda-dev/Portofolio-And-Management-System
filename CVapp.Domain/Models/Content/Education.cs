e022ca70a1765cc62b2c75372f0138cb5c75e14c
EvilPanda <joratractoristul@gmail.com>
2022-04-30 21:11:13 +0300

    Create education model

---
+++ CVapp.Domain/Models/Content/Education.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Domain.Models.Content
+{
+    public class Education : IEntityBase
+    {
+        public int Id { get; set; }
+        public int Date { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
