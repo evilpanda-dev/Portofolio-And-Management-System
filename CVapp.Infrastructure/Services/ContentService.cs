bfbcd5d9deebb59c3c79785309e585673b619849
EvilPanda <joratractoristul@gmail.com>
2022-04-30 23:11:02 +0300

    Add dependencies and add functionality to the new methods in content service

--- CVapp.Infrastructure/Services/ContentService.cs
+++ CVapp.Infrastructure/Services/ContentService.cs
@@ -3,6 +3,7 @@ using CVapp.Domain.Models.Content;
 using CVapp.Infrastructure.Abstractions;
 using CVapp.Infrastructure.DTOs;
 using CVapp.Infrastructure.Repository.EducationSectionRepository;
+using CVapp.Infrastructure.Repository.SkillRepository;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -14,20 +15,22 @@ namespace CVapp.Infrastructure.Services
 {
     public class ContentService : IContentService
     {
-        private readonly IEducationRepository _educationSectionRepository;
+        private readonly IEducationRepository _educationRepository;
+        private readonly ISkillRepository _skillRepository;
         private readonly IMapper _mapper;
-        
-        public ContentService(IEducationRepository educationSectionRepository,IMapper mapper)
+
+        public ContentService(IEducationRepository educationRepository, IMapper mapper, ISkillRepository skillRepository)
         {
-            _educationSectionRepository = educationSectionRepository;
+            _educationRepository = educationRepository;
             _mapper = mapper;
+            _skillRepository = skillRepository;
         }
         
         public IEnumerable<EducationDto>  GetEducationContent()
         {
             try
             {    
-            var educations = _educationSectionRepository.GetAllEducations();
+            var educations = _educationRepository.GetAllEducations();
                 var educationResult = _mapper.Map<IEnumerable<EducationDto>>(educations);
                 var educationResultArray = educationResult.ToArray();
                 
@@ -38,5 +41,45 @@ namespace CVapp.Infrastructure.Services
             }
             
         }
+
+        public IEnumerable<SkillDto> GetSkillContent()
+        {
+            try
+            {
+                var skills = _skillRepository.GetAllSkills();
+                var skillResult = _mapper.Map<IEnumerable<SkillDto>>(skills);
+                var skillResultArray = skillResult.ToArray();
+
+                return skillResultArray;
+            }
+            catch
+            {
+                throw new Exception("Error in getting education data");
+            }
+
+        }
+
+        public SkillDto AddNewSkill(SkillDto skillDto)
+        {
+            var skill = new Skill
+            {
+                Name = skillDto.Name,
+                Range = skillDto.Range
+            };
+            try
+            {
+                _skillRepository.Create(skill);
+            } catch
+            {
+                throw new Exception("Error in adding new skill");
+            }
+            return new SkillDto
+            {
+                Name = skill.Name,
+                Range = skill.Range
+            };
+        }
+        
+        
     }
 }
