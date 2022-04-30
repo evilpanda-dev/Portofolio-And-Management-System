a15ec1b139893cf66df35fb36dcbf91681d996ee
EvilPanda <joratractoristul@gmail.com>
2022-04-30 23:09:50 +0300

    Create migration for skill table

---
+++ CVapp.Infrastructure/Data/Migrations/20220430183254_CreateSkillContentTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CVapp.Migrations
+{
+    public partial class CreateSkillContentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    SkillId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SkillName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    SkillRange = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.SkillId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skills_SkillName",
+                table: "Skills",
+                column: "SkillName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Skills");
+        }
+    }
+}
