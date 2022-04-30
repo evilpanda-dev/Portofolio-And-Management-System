765d207ce6116d6dacbd9785710dd22af3329f93
EvilPanda <joratractoristul@gmail.com>
2022-04-30 21:10:38 +0300

    Create content controller and add get method for education section

---
+++ CVapp/Controllers/ContentController.cs
@@ -0,0 +1,25 @@
+﻿using CVapp.Infrastructure.Abstractions;
+using CVapp.Infrastructure.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVapp.API.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class ContentController : Controller
+    {
+        private readonly IContentService _contentService;
+
+        public ContentController(IContentService contentService)
+        {
+            _contentService = contentService;
+        }
+
+        [HttpGet("educations")]
+        public IActionResult GetEducationContent()
+        {
+            var data = _contentService.GetEducationContent();
+            return Ok(data);
+        }
+    }
+}
