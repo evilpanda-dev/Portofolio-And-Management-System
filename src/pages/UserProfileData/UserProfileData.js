a77c53032eecce7119625024bfbc76df561b96e2
EvilPanda <joratractoristul@gmail.com>
2022-05-01 23:24:54 +0300

    Add terminate account functionality for user profile data page

--- src/pages/UserProfileData/UserProfileData.js
+++ src/pages/UserProfileData/UserProfileData.js
@@ -5,12 +5,28 @@ import './UserProfileData.css';
 import { Typography } from "@mui/material";
 import '@fontsource/roboto/400.css';
 import Button from "../../components/Button/Button";
+import { UserContext } from "../../providers/UserProvider";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 const UserProfileData = props => {
     const { setImage, setUserName, setRole, imageSrc } = props;
+    const { user } = useContext(UserContext);
+    const navigate = useNavigate();
+    const userId = user?.userId;
+
     const buttonDesign = {
         buttonClass: "previousButton"
       }
+
+      const terminateAccount = () => {
+        fetch(`https://localhost:5000/api/deleteUser/${userId}`, {
+    method: 'DELETE'
+  })
+  .then(response => response.json());
+  navigate('/Inner')
+}
+
     return(
         <>
         <Panel/>
@@ -22,6 +38,9 @@ const UserProfileData = props => {
 <DataTable/>
 <div className="previousPageButtonContainer">
 <Button text="Previous page" address="/profile" theme={buttonDesign} />
+</div>
+<div className="terminateAccountButtonContainer">
+    <button onClick={terminateAccount}>Terminate account</button>
 </div>
             </section>
         </>
