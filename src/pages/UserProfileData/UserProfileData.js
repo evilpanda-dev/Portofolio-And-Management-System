f0bbbebcaa006762b31a22f558c3852ea39c5d7a
EvilPanda <joratractoristul@gmail.com>
2022-05-01 21:09:50 +0300

    Add button for going on previous page in user profile data page

--- src/pages/UserProfileData/UserProfileData.js
+++ src/pages/UserProfileData/UserProfileData.js
@@ -4,19 +4,25 @@ import Panel from "../../components/Panel/Panel";
 import './UserProfileData.css';
 import { Typography } from "@mui/material";
 import '@fontsource/roboto/400.css';
+import Button from "../../components/Button/Button";
 
 const UserProfileData = props => {
     const { setImage, setUserName, setRole, imageSrc } = props;
-
+    const buttonDesign = {
+        buttonClass: "previousButton"
+      }
     return(
         <>
         <Panel/>
-        <Header imageSrc={imageSrc} setUserName={setUserName} setRole={setRole} />
+        <Header imageSrc={imageSrc}/>
         <section className="userProfileData">
         <Typography variant="h4" component="h2" align="center" marginTop={'40px'}>
   Your profile data: 
 </Typography>
 <DataTable/>
+<div className="previousPageButtonContainer">
+<Button text="Previous page" address="/profile" theme={buttonDesign} />
+</div>
             </section>
         </>
     )
