22edfde4d243b934bc51e677cb811dc37c053995
EvilPanda <joratractoristul@gmail.com>
2022-05-01 20:24:10 +0300

    Add title to the user profile data page

--- src/pages/UserProfileData/UserProfileData.js
+++ src/pages/UserProfileData/UserProfileData.js
@@ -1,6 +1,9 @@
+import DataTable from "../../components/DataTable/DataTable";
 import Header from "../../components/Header/Header";
 import Panel from "../../components/Panel/Panel";
-import '../UserProfileData/UserProfileData.css';
+import './UserProfileData.css';
+import { Typography } from "@mui/material";
+import '@fontsource/roboto/400.css';
 
 const UserProfileData = props => {
     const { setImage, setUserName, setRole, imageSrc } = props;
@@ -10,7 +13,10 @@ const UserProfileData = props => {
         <Panel/>
         <Header imageSrc={imageSrc} setUserName={setUserName} setRole={setRole} />
         <section className="userProfileData">
-
+        <Typography variant="h4" component="h2" align="center" marginTop={'40px'}>
+  Your profile data: 
+</Typography>
+<DataTable/>
             </section>
         </>
     )
