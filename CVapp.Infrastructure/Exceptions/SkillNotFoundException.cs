7298b339a64a8d3d04ebbdd16f1ce31c17c486db
EvilPanda <joratractoristul@gmail.com>
2022-05-03 21:08:57 +0300

    Create skill not found exception

---
+++ CVapp.Infrastructure/Exceptions/SkillNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.Exceptions
+{
+    public class SkillNotFoundException : ApplicationException
+    {
+        public SkillNotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
