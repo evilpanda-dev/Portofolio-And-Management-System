5b7857b22c3cf696211408f796437687ea65ecfc
EvilPanda <joratractoristul@gmail.com>
2022-05-03 03:15:21 +0300

    Add confirm dialog in user profile data page

--- src/pages/UserProfileData/UserProfileData.js
+++ src/pages/UserProfileData/UserProfileData.js
@@ -8,24 +8,25 @@ import Button from "../../components/Button/Button";
 import { UserContext } from "../../providers/UserProvider";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import ConfirmDialog from "../../components/ConfirmDialog/ConfirmDialog";
 
 const UserProfileData = props => {
     const { setImage, setUserName, setRole, imageSrc } = props;
-    const { user } = useContext(UserContext);
-    const navigate = useNavigate();
-    const userId = user?.userId;
+    // const { user } = useContext(UserContext);
+    // const navigate = useNavigate();
+    // const userId = user?.userId;
 
     const buttonDesign = {
         buttonClass: "previousButton"
       }
 
-      const terminateAccount = () => {
-        fetch(`https://localhost:5000/api/deleteUser/${userId}`, {
-    method: 'DELETE'
-  })
-  .then(response => response.json());
-  navigate('/Inner')
-}
+//       const terminateAccount = () => {
+//         fetch(`https://localhost:5000/api/deleteUser/${userId}`, {
+//     method: 'DELETE'
+//   })
+//   .then(response => response.json());
+//   navigate('/Inner')
+// }
 
     return(
         <>
@@ -40,7 +41,8 @@ const UserProfileData = props => {
 <Button text="Previous page" address="/profile" theme={buttonDesign} />
 </div>
 <div className="terminateAccountButtonContainer">
-    <button onClick={terminateAccount}>Terminate account</button>
+    {/* <button onClick={terminateAccount}>Terminate account</button> */}
+<ConfirmDialog/>
 </div>
             </section>
         </>
