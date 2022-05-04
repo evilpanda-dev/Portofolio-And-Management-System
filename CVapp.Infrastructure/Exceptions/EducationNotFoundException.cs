a66ddd5e67d32a50e1521ed4793eef57a443f974
EvilPanda <joratractoristul@gmail.com>
2022-05-04 22:50:08 +0300

    Create education not found exception

---
+++ CVapp.Infrastructure/Exceptions/EducationNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.Exceptions
+{
+    public class EducationNotFoundException : ApplicationException
+    {
+        public EducationNotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
