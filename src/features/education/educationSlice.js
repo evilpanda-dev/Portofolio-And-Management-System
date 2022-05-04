e5599f29cdb9fed3e8948f2fc7e3d2c9fef5e312
EvilPanda <joratractoristul@gmail.com>
2022-05-04 22:53:28 +0300

    Create async thunks in education slice for crud operations in education component

--- src/features/education/educationSlice.js
+++ src/features/education/educationSlice.js
@@ -19,6 +19,88 @@ export const fetchEducations = createAsyncThunk(
   }
 );
 
+export const addNewEducation = createAsyncThunk(
+  "educations/addNewEducation",
+  async (educationData, { rejectWithValue,dispatch}) => {
+    const { educationDate, educationTitle,educationDescription } = educationData;
+    try {
+      const education = {
+        date: educationDate,
+        title: educationTitle,
+        text : educationDescription
+      };
+      const response = await fetch("https://localhost:5000/api/addEducation", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(education),
+      });
+
+      if (!response.ok) {
+        throw new Error("Can't add skill. Server error");
+      }
+
+      const data = await response.json();
+      dispatch(setEducation(data));
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const updateEducation = createAsyncThunk(
+  "educations/updateEducation",
+  async (educationData, { rejectWithValue,dispatch }) => {
+    const { educationId,educationDate, educationTitle,educationDescription } = educationData;
+    try {
+      // const education = {
+      //   date: educationDate,
+      //   title: educationTitle,
+      //   text : educationDescription
+      // };
+      fetch(`https://localhost:5000/api/updateEducation/${educationId}`, {
+          method:"PATCH",
+          body:JSON.stringify({
+            date: educationDate,
+        title: educationTitle,
+        text : educationDescription,
+         }),
+          
+          headers: new Headers({
+            "Content-Type": "application/json",
+          }),
+          
+            })
+            dispatch(updateEducationState({date:educationDate,title:educationTitle,text:educationDescription}));
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const removeEducation = createAsyncThunk(
+  "educations/removeEducation",
+  async (educationData, { rejectWithValue,dispatch}) => {
+    const {educationId, educationDate, educationTitle,educationDescription } = educationData;
+    try {
+      // const education = {
+      //   date: educationDate,
+      //   title: educationTitle,
+      //   text : educationDescription
+      // };
+     
+ await fetch(`https://localhost:5000/api/deleteEducation/${educationId}`, {
+        method: 'DELETE'
+      })
+     // .then(response => response.json());
+     dispatch(deleteEducation(educationId));
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
 const setError = (state, action) => {
   state.status = "rejected";
   state.error = action.payload;
@@ -31,6 +113,29 @@ export const educationSlice = createSlice({
     status: null,
     error: null,
   },
+  reducers: {
+    setEducation: (state, action) => {
+      state.educationList.push(action.payload);
+    },
+    deleteEducation:(state,action) =>{
+      //find index of the value for being deleted
+      const index = state.educationList.findIndex(education => education.id === action.payload);
+      //remove the value from the array
+      if(index !== -1){
+        state.educationList.splice(index,1);
+      }
+    },
+    updateEducationState:(state,action) =>{
+     // console.log(action.payload)
+      state.educationList.map((education)=>{
+        if(education.id === action.payload.id){
+          education.date = action.payload.date;
+          education.title = action.payload.title;
+          education.text = action.payload.text;
+        }
+      })
+  }
+},
   extraReducers: {
     [fetchEducations.pending]: (state, action) => {
       state.status = "loading";
@@ -43,5 +148,5 @@ export const educationSlice = createSlice({
     [fetchEducations.rejected]: setError,
   },
 });
-
+const { setEducation,updateEducationState,deleteEducation } = educationSlice.actions;
 export default educationSlice.reducer;
