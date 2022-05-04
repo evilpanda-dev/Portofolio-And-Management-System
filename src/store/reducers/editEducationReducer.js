5766a436996cedd71208aa27496a60eb371fdad2
EvilPanda <joratractoristul@gmail.com>
2022-05-04 17:57:42 +0300

    Create editEducationReducer for managing the state of the edit button for education component

---
+++ src/store/reducers/editEducationReducer.js
@@ -0,0 +1,18 @@
+const defaultState = {
+    editEducation: false,
+  };
+  
+  const EDITEDUCATION_ACTIVATED = "EDITEDUCATION_ACTIVATED";
+  const EDITEDUCATION_DEACTIVATED = "EDITEDUCATION_DEACTIVATED";
+  
+  export const editEducationReducer = (state = defaultState, action) => {
+    switch (action.type) {
+      case EDITEDUCATION_ACTIVATED:
+        return { ...state, editEducation: action.payload };
+      case EDITEDUCATION_DEACTIVATED:
+        return { ...state, editEducation: action.payload };
+      default:
+        return state;
+    }
+  };
+  
