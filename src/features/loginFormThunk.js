2088ad0f722aae8c4857af4f04400adbb09435d0
EvilPanda <joratractoristul@gmail.com>
2022-05-07 00:21:40 +0300

    Create an async thunk for login form

---
+++ src/features/loginFormThunk.js
@@ -0,0 +1,28 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const loginUser = createAsyncThunk(
+    "login/loginUser",
+    async (loginData, { rejectWithValue}) => {
+      const {email,password } = loginData;
+      try {
+        const userToLogin = {
+          email: email,
+          password : password
+        };
+        const response = await fetch("https://localhost:5000/api/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include",
+            body: JSON.stringify(userToLogin),
+          })
+  
+    const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.Message);
+        }
+
+      } catch (error) {
+        return rejectWithValue(error.message);
+      }
+    }
+  );
