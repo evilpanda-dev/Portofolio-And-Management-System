7afbedff99917a2eea99d2a2a038c0b5340b365b
EvilPanda <joratractoristul@gmail.com>
2022-05-07 02:03:17 +0300

    Add dispatch terminate event in confirm dialog component

--- src/components/ConfirmDialog/ConfirmDialog.js
+++ src/components/ConfirmDialog/ConfirmDialog.js
@@ -7,11 +7,15 @@ import { Dialog,
 import { useState,useContext } from 'react';
 import { UserContext } from '../../providers/UserProvider';
 import { useNavigate } from 'react-router-dom';
-
+import {useDispatch} from 'react-redux';
+import {AlertContext} from '../../providers/AlertProvider';
+import AlertWindow from '../AlertWindow/AlertWindow';
 const ConfirmDialog = () => {
     const [open, setOpen] = useState(false);
     const { user } = useContext(UserContext);
+    const {setAlert} = useContext(AlertContext);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const userId = user?.userId;
 
     const handleClickToOpen = () => {
@@ -23,10 +27,33 @@ const ConfirmDialog = () => {
     };
 
     const terminateAccount = () => {
-        fetch(`https://localhost:5000/api/deleteUser/${userId}`, {
+       const response = fetch(`https://localhost:5000/api/deleteUser/${userId}`, {
     method: 'DELETE'
   })
-  .then(response => response.json());
+  .then(response => {
+          if (response.status === 200) {
+
+    setAlert({appAlerts:
+            alert = (
+              // showAlertWindow("success","Account created successfully",true)
+              <AlertWindow message="Account successeful terminated!" alertType="success"/>
+            )})
+          }
+          else {
+            //return response.text().then(text => { throw new Error(text) })
+            return response.json().then(text => { throw new Error(text.Message) })
+          }
+        })
+        .catch(error => {
+    
+          // console.log('caught it!',error.message);
+          setAlert({appAlerts:
+            alert = (
+            // showAlertWindow("error",error.message,true)
+            <AlertWindow message={error.message} alertType="error" />
+          )})
+        })
+    dispatch({type:"WINDOW_ACTIVATED",payload:true});
   navigate('/Inner')
 }
     
