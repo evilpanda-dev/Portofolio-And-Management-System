f6491e099488cfe7aa6f452bacc730636377fda6
EvilPanda <joratractoristul@gmail.com>
2022-05-07 00:22:14 +0300

    Add alert provider in the index

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import UserProvider from "./providers/UserProvider";
 import UserProfileProvider from "./providers/UserProfileProvider";
+import AlertProvider from "./providers/AlertProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <UserProvider>
       <UserProfileProvider>
+        <AlertProvider>
     <App />
+    </AlertProvider>
     </UserProfileProvider>
     </UserProvider>
   </Router>
