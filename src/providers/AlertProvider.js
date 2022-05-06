a93bffae1c1e9457563c10884b3516784f3cee1c
EvilPanda <joratractoristul@gmail.com>
2022-05-07 00:20:47 +0300

    Create AlertProvider to have alert context in the app

---
+++ src/providers/AlertProvider.js
@@ -0,0 +1,11 @@
+import { createContext,useState } from "react";
+
+export const AlertContext = createContext({})
+
+const AlertProvider=({children}) =>{
+const [alert,setAlert]=useState({})
+
+return <AlertContext.Provider value={{alert,setAlert}}>{children}</AlertContext.Provider>
+}
+
+export default AlertProvider
