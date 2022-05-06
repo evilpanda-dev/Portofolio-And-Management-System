e9aa82b4907c5e1bb274edaafcae6b459109c171
EvilPanda <joratractoristul@gmail.com>
2022-05-07 00:26:20 +0300

    Add alert and dispatch fetch api in logout component

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,19 +1,50 @@
 import "../Logout/Logout.css";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../features/logoutThunk";
+import { AlertContext } from "../../providers/AlertProvider";
+import { useContext } from "react";
+import AlertWindow from "../AlertWindow/AlertWindow";
 
 const Logout = (props) => {
   const { setUserName, setRole } = props;
   let navigate = useNavigate();
-
+ const dispatch = useDispatch();
+const {setAlert} = useContext(AlertContext)
   const logout = async () => {
-    await fetch("https://localhost:5000/api/logout", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      credentials: "include",
-    });
-    setUserName("");
+    dispatch(logoutUser())
+    .then((data) => {
+      if(data.meta.requestStatus == "fulfilled"){
+        setUserName("");
     setRole("");
     navigate("/Inner")
+        setAlert({appAlerts:
+          alert = (
+          <AlertWindow message="You successefully logged in" alertType="success"/>
+        )})
+      } 
+      else {
+        throw new Error(data.payload)
+      }
+      })
+      .catch(error => {
+      
+      // console.log('caught it!',error.message);
+      setAlert({appAlerts:
+        alert = (
+        // showAlertWindow("error",error.message,true)
+        <AlertWindow message={error.message} alertType="error" />
+      )})
+      })
+      dispatch({type:"WINDOW_ACTIVATED",payload:true})
+    // await fetch("https://localhost:5000/api/logout", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   credentials: "include",
+    // });
+    // setUserName("");
+    // setRole("");
+    // navigate("/Inner")
   };
 
   return (
