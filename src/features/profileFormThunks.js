a44288b890755d051519a49c0fe257e36bfa1ffe
EvilPanda <joratractoristul@gmail.com>
2022-05-07 02:02:25 +0300

    Create async thunks for profile form component

---
+++ src/features/profileFormThunks.js
@@ -0,0 +1,64 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const updateUserProfile = createAsyncThunk(
+    "update/userProfile",
+    async (userProfileData, { rejectWithValue}) => {
+      const {userId,firstName,lastName,birthDate,address,city,country,phoneNumber,aboutMe } = userProfileData;
+      try {
+        const profileToUpdate = {
+            firstName : firstName,
+            lastName : lastName,
+            birthDate : birthDate,
+            address : address,
+            city : city,
+            country : country,
+            phoneNumber : phoneNumber,
+            aboutMe : aboutMe
+        };
+
+     const response = await fetch(`https://localhost:5000/api/updateProfile/${userId}`, {
+      method:"PATCH",
+      body: JSON.stringify(profileToUpdate),
+      
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      
+        })
+  
+   // const data = await response.json();
+        if (!response.ok) {
+          throw new Error(response.Message);
+        }
+
+      } catch (error) {
+        return rejectWithValue(error.message);
+      }
+    }
+  );
+
+
+  export const uploadProfileAvatar = createAsyncThunk(
+    "avatar/uploadProfileAvatar",
+    async (avatarData, { rejectWithValue}) => {
+      const {avatar} = avatarData;
+      try {
+        const response = await fetch("https://localhost:5000/api/saveAvatar", {
+          method: "POST",
+          body: avatar,
+          headers: new Headers({
+            Accept: "application/json",
+          }),
+          credentials: "include",
+        })
+  
+   // const data = await response.json();
+        if (!response.ok) {
+          throw new Error(response.Message);
+        }
+
+      } catch (error) {
+        return rejectWithValue(error.message);
+      }
+    }
+  );
