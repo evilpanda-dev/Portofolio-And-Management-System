8f69620859f37b6b3dc73a0bf0200dcbfe536dbf
EvilPanda <joratractoristul@gmail.com>
2022-05-07 22:04:02 +0300

    Create newsletter reducer for managing the newsletter's state

---
+++ src/store/reducers/newsLetterReducer.js
@@ -0,0 +1,18 @@
+const defaultState = {
+    isNewsletter: false,
+  };
+  
+  const SHOW_NEWSLETTER = "SHOW_NEWSLETTER";
+  const HIDE_NEWSLETTER = "HIDE_NEWSLETTER";
+  
+  export const newsLetterReducer = (state = defaultState, action) => {
+    switch (action.type) {
+      case SHOW_NEWSLETTER:
+        return { ...state, isNewsletter: action.payload };
+      case HIDE_NEWSLETTER:
+        return { ...state, isNewsletter: action.payload };
+      default:
+        return state;
+    }
+  };
+  
