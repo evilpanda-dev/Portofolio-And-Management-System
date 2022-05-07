acc2551d321b40d9b6312fa89507caa4cf751cbe
EvilPanda <joratractoristul@gmail.com>
2022-05-07 22:06:11 +0300

    Create newsletter DTO

---
+++ CVapp.Infrastructure/DTOs/NewsletterDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.DTOs
+{
+    public class NewsletterDto
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public int UserId { get; set; }
+    }
+}
