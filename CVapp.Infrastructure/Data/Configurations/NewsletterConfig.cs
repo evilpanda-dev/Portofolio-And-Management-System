a6e674e9d70c7a6cc9994b21000b37967e18ad37
EvilPanda <joratractoristul@gmail.com>
2022-05-07 22:07:17 +0300

    Configure the newsletter model for the database

---
+++ CVapp.Infrastructure/Data/Configurations/NewsletterConfig.cs
@@ -0,0 +1,26 @@
+﻿using CVapp.Domain.Models.Content;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.Data.Configurations
+{
+    public class NewsletterConfig : IEntityTypeConfiguration<Newsletter>
+    {
+        public void Configure(EntityTypeBuilder<Newsletter> builder)
+        {
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Id).ValueGeneratedOnAdd()
+                .HasColumnType("int");
+            builder.Property(u => u.Email)
+                .HasColumnType("nvarchar")
+                .HasMaxLength(20)
+                .HasColumnName("Email");
+        }
+    }
+}
+        
