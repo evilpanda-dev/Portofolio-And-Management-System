439d716662af2b3c1701f5d034a48a1ff644d711
EvilPanda <joratractoristul@gmail.com>
2022-05-07 22:06:38 +0300

    Create the newsletter repository for all the related methods

---
+++ CVapp.Infrastructure/Repository/NewsletterRepository/INewsletterRepository.cs
@@ -0,0 +1,15 @@
+﻿using CVapp.Domain.Models.Content;
+using CVapp.Infrastructure.Repository.GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.Repository.NewsletterRepository
+{
+    public interface INewsletterRepository : IRepository<Newsletter>
+    {
+        public Newsletter GetByUserId(int userId);
+    }
+}
---
+++ CVapp.Infrastructure/Repository/NewsletterRepository/NewsletterRepository.cs
@@ -0,0 +1,70 @@
+﻿using CVapp.Domain.Models.Content;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.Repository.NewsletterRepository
+{
+    public class NewsletterRepository : INewsletterRepository
+    {
+        private readonly DbContext _context;
+        private DbSet<Newsletter> _dbSet;
+
+        public NewsletterRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<Newsletter>();
+        }
+
+        public Newsletter Create(Newsletter entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+
+            return entity;
+        }
+
+        public void Delete(Newsletter entity)
+        {
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var entityToDelete = _dbSet.FirstOrDefault(e => e.Id == id);
+            if (entityToDelete != null)
+            {
+                _dbSet.Remove(entityToDelete);
+                _context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Newsletter> Filter() => _dbSet;
+
+
+        public IEnumerable<Newsletter> Filter(Func<Newsletter, bool> predicate) => _dbSet.Where(predicate);
+        public Newsletter GetById(int id)
+        {
+            return _dbSet.FirstOrDefault(e => e.Id == id);
+        }
+
+        public Newsletter GetByUserId(int id)
+        {
+            return _dbSet.FirstOrDefault(e => e.UserId == id);
+        }
+
+        public void SaveChanges() => _context.SaveChanges();
+
+        public Newsletter Update(Newsletter entity)
+        {
+            _dbSet.Update(entity);
+            _context.SaveChanges();
+
+            return entity;
+        }
+    }
+}
