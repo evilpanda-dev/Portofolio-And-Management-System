8bc5947a5492b43dcc74736cd5bfab8f0fb00582
EvilPanda <joratractoristul@gmail.com>
2022-05-07 22:05:58 +0300

    Create the newsletter model

---
+++ CVapp.Domain/Models/Content/Newsletter.cs
@@ -0,0 +1,18 @@
+﻿using CVapp.Domain.Models.Authentification;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Domain.Models.Content
+{
+    public class Newsletter : IEntityBase
+    {
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
