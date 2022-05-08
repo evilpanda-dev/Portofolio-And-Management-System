d469deb9c6c44c2712c7a5197f74a0466b57bd3a
EvilPanda <joratractoristul@gmail.com>
2022-05-08 23:35:04 +0300

    Create getDate helper for changing the date from usual appearence to a normal one for front

---
+++ src/helpers/getDate.js
@@ -0,0 +1,8 @@
+export const getDate = (date) =>{
+    let dateTime = new Date(date)
+    if(date !== null){
+        return `${dateTime.getDate()}-${dateTime.getMonth() + 1}-${dateTime.getFullYear()}`
+    } else {
+        return null
+    }
+}
