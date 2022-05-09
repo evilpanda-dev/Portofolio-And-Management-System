b71fab4cc671041f6de8fb49a5d0976b548bb46b
EvilPanda <joratractoristul@gmail.com>
2022-05-09 18:08:40 +0300

    Create comment controller for all the endpoints needed for the app

---
+++ CVapp/Controllers/CommentController.cs
@@ -0,0 +1,54 @@
+﻿using CVapp.Infrastructure.Abstractions;
+using CVapp.Infrastructure.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVapp.API.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class CommentController : Controller
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpGet("comments")]
+        public IActionResult GetComments()
+        {
+            var data = _commentService.GetAllComments();
+            return Ok(data);
+        }
+
+        [HttpPost("addComment")]
+        public IActionResult AddComment(CommentDto commentDto)
+        {
+            var data = _commentService.AddNewComment(commentDto);
+            return Ok(data);
+        }
+
+        [HttpPatch("updateComment/{id}")]
+        public IActionResult UpdateComment(int id, CommentDto commentDto)
+        {
+            var data = _commentService.UpdateComment(id, commentDto);
+            return Ok(data);
+        }
+
+        [HttpDelete("deleteComment/{id}")]
+        public IActionResult DeleteEducationContent(int id)
+        {
+            _commentService.DeleteComment(id);
+            return Ok();
+        }
+
+        [HttpPost("replyComment")]
+        public IActionResult ReplyComment(CommentDto commentDto)
+        {
+            var data = _commentService.ReplyToTheComment(commentDto);
+            return Ok(data);
+        }
+    }
+
+}
