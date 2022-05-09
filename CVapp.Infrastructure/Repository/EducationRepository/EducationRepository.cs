801b97b437539a95e7e958d9716d07d9eb1f3f98
EvilPanda <joratractoristul@gmail.com>
2022-05-09 18:08:55 +0300

    Sync namespaces in education repository

--- CVapp.Infrastructure/Repository/EducationRepository/EducationRepository.cs
+++ CVapp.Infrastructure/Repository/EducationRepository/EducationRepository.cs
@@ -7,9 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CVapp.Infrastructure.Repository.EducationSectionRepository
+namespace CVapp.Infrastructure.Repository.EducationRepository
 {
-    public class EducationRepository : Repository<Education>,IEducationRepository
+    public class EducationRepository : Repository<Education>, IEducationRepository
     {
         private readonly DbContext _context;
         private DbSet<Education> _dbSet;
@@ -19,49 +19,49 @@ namespace CVapp.Infrastructure.Repository.EducationSectionRepository
             _context = context;
             _dbSet = _context.Set<Education>();
         }
-        
-       /* public Education Create(Education entity)
-        {
-            _dbSet.Add(entity);
-            _context.SaveChanges();
 
-            return entity;
-        }
+        /* public Education Create(Education entity)
+         {
+             _dbSet.Add(entity);
+             _context.SaveChanges();
 
-        public void Delete(Education entity)
-        {
-            _dbSet.Remove(entity);
-            _context.SaveChanges();
-        }
+             return entity;
+         }
 
-        public void Delete(int id)
-        {
-            var entityToDelete = _dbSet.FirstOrDefault(e => e.Id == id);
-            if (entityToDelete != null)
-            {
-                _dbSet.Remove(entityToDelete);
-                _context.SaveChanges();
-            }
-        }
+         public void Delete(Education entity)
+         {
+             _dbSet.Remove(entity);
+             _context.SaveChanges();
+         }
 
-        public IEnumerable<Education> Filter() => _dbSet;
+         public void Delete(int id)
+         {
+             var entityToDelete = _dbSet.FirstOrDefault(e => e.Id == id);
+             if (entityToDelete != null)
+             {
+                 _dbSet.Remove(entityToDelete);
+                 _context.SaveChanges();
+             }
+         }
 
-        public IEnumerable<Education> Filter(Func<Education, bool> predicate) => _dbSet.Where(predicate);
+         public IEnumerable<Education> Filter() => _dbSet;
 
-        public Education GetById(int id)
-        {
-            return _dbSet.FirstOrDefault(e => e.Id == id);
-        }
+         public IEnumerable<Education> Filter(Func<Education, bool> predicate) => _dbSet.Where(predicate);
 
-        public void SaveChanges() => _context.SaveChanges();
+         public Education GetById(int id)
+         {
+             return _dbSet.FirstOrDefault(e => e.Id == id);
+         }
 
-        public Education Update(Education entity)
-        {
-            _dbSet.Update(entity);
-            _context.SaveChanges();
+         public void SaveChanges() => _context.SaveChanges();
 
-            return entity;
-        }*/
+         public Education Update(Education entity)
+         {
+             _dbSet.Update(entity);
+             _context.SaveChanges();
+
+             return entity;
+         }*/
 
         public IEnumerable<Education> GetAllEducations()
         {
--- CVapp.Infrastructure/Repository/EducationRepository/IEducationRepository.cs
+++ CVapp.Infrastructure/Repository/EducationRepository/IEducationRepository.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CVapp.Infrastructure.Repository.EducationSectionRepository
+namespace CVapp.Infrastructure.Repository.EducationRepository
 {
     public interface IEducationRepository
     {
