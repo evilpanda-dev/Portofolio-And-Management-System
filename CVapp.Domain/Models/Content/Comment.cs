6fac1ce48ab1789e524366f6433c0ed7ec3f6789
EvilPanda <joratractoristul@gmail.com>
2022-05-10 02:55:12 +0300

    Add user avatar inside comment model

--- CVapp.Domain/Models/Content/Comment.cs
+++ CVapp.Domain/Models/Content/Comment.cs
@@ -10,6 +10,7 @@ namespace CVapp.Domain.Models.Content
     public class Comment : IEntityBase
     {
         public int Id { get; set; }
+        public byte[]? Image { get; set; }
         public string Text { get; set; }
         public string UserName { get; set; }
         public int UserId { get; set; }
