b846ee79426444972674e0ebd389b303dc3a1366
EvilPanda <joratractoristul@gmail.com>
2022-05-10 02:52:33 +0300

    Add comments to the feedback component

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,12 +1,18 @@
+import { useContext } from "react";
+import { UserContext } from "../../providers/UserProvider";
 import Info from "../Info/Info";
+import Comments from "./Comments";
 
 const Feedback = (props) => {
-  const { data } = props;
+  const { imageSrc } = props;
+  const {user} = useContext(UserContext)
+
   return (
     <>
       <section id="feedBack">
         <h1 className="feedbackSection">Feedbacks</h1>
-        {data.map((user) => (
+        <Comments currentUserId = {user.userId} currentUserName={user.userName} currentAvatar ={imageSrc}/>
+        {/* {data.map((user) => (
           <div className="feedbackWrapper" key={user.reporter.name}>
             <Info text={user.feedback} />
             <div className="userDetails">
@@ -22,7 +28,7 @@ const Feedback = (props) => {
               </a>
             </div>
           </div>
-        ))}
+        ))} */}
       </section>
     </>
   );
