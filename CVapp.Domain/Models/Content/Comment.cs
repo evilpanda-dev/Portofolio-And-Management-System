5c7becf9deb73b00fd2b522363564d317a797e8b
EvilPanda <joratractoristul@gmail.com>
2022-05-09 18:06:05 +0300

    Create comment model

---
+++ CVapp.Domain/Models/Content/Comment.cs
@@ -0,0 +1,20 @@
+﻿using CVapp.Domain.Models.Authentification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Domain.Models.Content
+{
+    public class Comment : IEntityBase
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string UserName { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public int? ParentId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
