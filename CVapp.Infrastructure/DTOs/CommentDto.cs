a08423b0f61b98043384d027daa1a68bc3ca573c
EvilPanda <joratractoristul@gmail.com>
2022-05-10 02:56:22 +0300

    Add user avatar inside comment dto

--- CVapp.Infrastructure/DTOs/CommentDto.cs
+++ CVapp.Infrastructure/DTOs/CommentDto.cs
@@ -9,6 +9,7 @@ namespace CVapp.Infrastructure.DTOs
     public class CommentDto
     {
         public int Id { get; set; }
+        public byte[]? Image { get; set; }
         public string Text { get; set; }
         public string UserName { get; set; }
         public int UserId { get; set; }
