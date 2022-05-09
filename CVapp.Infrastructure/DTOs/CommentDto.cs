e39f32f82910dd7cc8146ddc4396a1e2fe71880a
EvilPanda <joratractoristul@gmail.com>
2022-05-09 18:06:14 +0300

    Create comment dto

---
+++ CVapp.Infrastructure/DTOs/CommentDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.DTOs
+{
+    public class CommentDto
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string UserName { get; set; }
+        public int UserId { get; set; }
+        public int? ParentId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
