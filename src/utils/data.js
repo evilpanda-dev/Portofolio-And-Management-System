5db6af84ea902004d49369f4b5d93add8c2a1bba
EvilPanda <joratractoristul@gmail.com>
2022-05-09 16:09:17 +0300

    Add data for portofolio component

---
+++ src/utils/data.js
@@ -0,0 +1,43 @@
+import card1 from '../assets/images/card_1.png'
+import card3 from '../assets/images/card_3.png'
+
+const ProjectData = [
+    {
+
+        title: "Project 1",
+        text: "Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo",
+        url: "https://somesite.com",
+        image: card1,
+        category: "Code"
+    },
+
+    {
+
+        "title": "Project 2",
+        "text": "Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo",
+        url: "https://somesite.com",
+        image: card1,
+        category: "Code"
+    },
+
+
+    {
+        "title": "Project 3",
+        "text": "Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo",
+        url: "https://somesite.com",
+        image: card3,
+        category: "UI"
+    },
+
+
+    {
+        "title": "Project 4",
+        "text": "Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo",
+        url: "https://somesite.com",
+        image: card3,
+        category: "UI"
+    },
+
+]
+
+export default ProjectData
