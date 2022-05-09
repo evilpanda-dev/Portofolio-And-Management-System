5189f51b5b69e9abf9d71f695d2106dfeabd99b5
EvilPanda <joratractoristul@gmail.com>
2022-05-09 18:08:25 +0300

    Create comment not found exception

---
+++ CVapp.Infrastructure/Exceptions/CommentNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.Exceptions
+{
+    public class CommentNotFoundException : ApplicationException
+    {
+        public CommentNotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
