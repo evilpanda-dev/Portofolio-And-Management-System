c315a538efde1de941f8137e029317bb16a730e5
EvilPanda <joratractoristul@gmail.com>
2022-05-12 20:04:44 +0300

    Create charts component for mapping and filtering wakatime charts

---
+++ src/components/Charts/Charts.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import FilterButton from "../Button/FilterButton";
+import Chart from "./Chart";
+
+const Charts = props =>{
+    const { data } = props;
+
+  const allCategories = ["All", ...new Set(data.map((item) => item.category))];
+
+  const [menuItems, setMenuItems] = useState(data);
+  const [categories] = useState(allCategories);
+
+  const filterItem = (category) => {
+    if (category === "All") {
+      setMenuItems(data);
+      return;
+    }
+    const newItem = data.filter((item) => item.category === category);
+    setMenuItems(newItem);
+  };
+    return (
+<>
+        <h1 className="statsTitle">Developer stats</h1>
+        <div >
+          <FilterButton categories={categories} filterItem={filterItem} />
+        </div>
+
+        <div className="row">
+          {menuItems.map((project) => (
+            <Chart
+              url={project.src}
+              key={project.id}
+            />
+          ))}
+           </div>
+</>
+    )
+}
+
+export default Charts
