604a74c30b6accac87a19dc9392ca69e8ffc6d27
EvilPanda <joratractoristul@gmail.com>
2022-05-12 20:05:28 +0300

    Add panel,header and charts on the dashboard page

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,6 +1,21 @@
-const Dashboard = () =>{
+import Header from "../../components/Header/Header";
+import Panel from "../../components/Panel/Panel";
+import Portofolio from "../../components/Portofolio/Portofolio";
+import './Dashboard.css';
+import WakatimeData from '../../utils/wakaTimeData.js'
+import Charts from "../../components/Charts/Charts";
+
+const Dashboard = props =>{
+    const {
+        imageSrc
+     } = props
     return(
         <>
+        <Panel/>
+        <Header imageSrc={imageSrc}/>
+        <section className="dashboardPage">
+       <Charts data={WakatimeData}/>
+        </section>
         </>
     )
 }
