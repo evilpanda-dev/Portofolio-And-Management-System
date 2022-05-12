3cea09b15870abef2dc17545720bfbed07b86f63
EvilPanda <joratractoristul@gmail.com>
2022-05-12 20:04:23 +0300

    Create chart component for wakatime data

---
+++ src/components/Charts/Chart.js
@@ -0,0 +1,19 @@
+import "../Portofolio/PortofolioInfo.css";
+
+const Chart = (props) => {
+  const { url} = props;
+
+  return (
+    <>
+      <div className="column">
+        <div className="container">
+        <figure className="chart">
+            <embed src ={url}></embed>
+        </figure>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Chart;
