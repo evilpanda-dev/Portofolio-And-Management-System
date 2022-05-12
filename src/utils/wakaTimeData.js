618df5991338e11acb68b97f95f509c798938536
EvilPanda <joratractoristul@gmail.com>
2022-05-12 20:05:06 +0300

    Create a constant for storing data as json for charts component

---
+++ src/utils/wakaTimeData.js
@@ -0,0 +1,85 @@
+const WakatimeData = [
+    {
+        "id" : "1",
+        "src" : "https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/09a579dc-0a46-4dca-b674-2331751ada2b.svg",
+        "category" : "Last 7 days"
+    },
+    {
+        "id" : "2",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/9b5be007-1543-482b-bb1a-5d953aa572e8.svg",
+        "category" : "Last 7 days"
+    },
+    {
+        "id" : "3",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/ff6a2baa-c4af-45a5-9cf9-5a0dc7b22fec.svg",
+        "category" : "Last 7 days"
+    },
+    {
+        "id" : "4",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/e2355cf2-66d0-4fa7-a52c-fe272c15ff4e.svg",
+        "category" : "Last 7 days"
+    },
+    {
+        "id" : "5",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/1a333da4-b6b2-4e54-9be3-e74ebb9f4588.svg",
+        "category" : "Last 30 days"
+    },
+    {
+        "id" : "6",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/6244b238-863d-4e6a-bfee-a23a355bd938.svg",
+        "category" :  "Last 30 days"
+    },
+    {
+        "id" : "7",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/22f03906-6511-4a1f-b3a7-fbd2f27940b6.svg",
+        "category" : "Last 30 days"
+    },
+    {
+        "id" : "8",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/7b26a87c-f027-43e2-bf19-a0cffd2d2c78.svg",
+        "category" :  "Last 30 days"
+    },
+    {
+        "id" : "9",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/81a5fb58-724d-4405-8dd3-06e9057f8b56.svg",
+        "category" : "Last year"
+    },
+    {
+        "id" : "10",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/8dc6177a-9edd-416e-84e0-eeed80c91244.svg",
+        "category" : "Last year"
+    },
+    {
+        "id" : "11",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/ca55e36b-d25c-4bf3-af90-1311d66967b9.svg",
+        "category" : "Last year"
+    },
+    {
+        "id" : "12",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/dc79401a-15a9-4ef0-bdf3-80261f8a5d39.svg",
+        "category" : "Last year"
+    },
+    {
+        "id" : "13",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/fd22287e-c845-4ea4-81eb-396c652cf3fb.svg",
+        "category" : "All time"
+    },
+    {
+        "id" : "14",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/91cd659a-592f-4381-8dd4-b48eb5f7e32c.svg",
+        "category" : "All time"
+    },
+    {
+        "id" : "15",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/d665eb9b-e416-4636-a926-0ad12fab34e4.svg",
+        "category" : "All time"
+    },
+    {
+        "id" : "16",
+        "src" :"https://wakatime.com/share/@69fce20a-397c-45e6-a804-382bc7f03dfc/a77191f5-7e77-4d1b-a017-ea8aec678e87.svg",
+        "category" : "All time"
+    }
+
+]
+
+export default WakatimeData;
