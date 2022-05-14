ea5bc8bb7d01b406debe1c06d334cfbd24520488
EvilPanda <joratractoristul@gmail.com>
2022-05-14 20:12:00 +0300

    Create the environment for a new function for getting data from database for transactions component

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,16 +1,23 @@
 import { useState } from "react";
-import { generateAndDownloadExcel,saveToDatabase } from "../../functions/excelUtils"
+import { generateAndDownloadExcel,saveToDatabase } from "../../api/displayDataApi"
 import UploadFile from "./UploadFile.js"
 import DisplayData from "./DisplayData.js";
+import { getDatabaseData } from "../../api/displayDataApi.js";
 
 const Transactions = () => {
     const [uploadedExcelData, setUploadedExcelData] = useState([]);
     const [dataForDb, setDataForDb] = useState([]);
+    const [isViewingData, setIsViewingData] = useState(false);
+
   const uploadedExcelDataHandler = (data) => {
     setUploadedExcelData(data);
   };
   
-  // console.log(dataForDb)
+  const viewDatabaseTable = () => {
+    setIsViewingData(true);
+    getDatabaseData()
+  }
+
     return(
         <>
         <h1 className="transactionsTitle">Transactions</h1>
@@ -20,6 +27,7 @@ const Transactions = () => {
         <button onClick={()=>{
             saveToDatabase(dataForDb)
         }}>Save to database</button>
+        <button onClick={viewDatabaseTable}>View database table</button>
         <div>
         <UploadFile onUploadExcelFile={uploadedExcelDataHandler} />
       <DisplayData excelData={uploadedExcelData} setData={setDataForDb}/>
