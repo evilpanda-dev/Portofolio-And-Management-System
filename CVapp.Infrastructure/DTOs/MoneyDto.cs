4ffe380c7f2978f9fb923070247331e71b80e488
EvilPanda <joratractoristul@gmail.com>
2022-05-14 16:58:23 +0300

    Create money dto

---
+++ CVapp.Infrastructure/DTOs/MoneyDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.DTOs
+{
+    public class MoneyDto
+    {
+        public int Id { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string TransactionAccount { get; set; }
+        public string Category { get; set; }
+        public string Item { get; set; }
+        public float Amount { get; set; }
+        public string TransactionType { get; set; }
+        public string Currency { get; set; }
+    }
+}
