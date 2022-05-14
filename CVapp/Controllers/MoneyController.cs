555fc59a79be47d5c1e07f3ed5f6987e7575465b
EvilPanda <joratractoristul@gmail.com>
2022-05-14 17:01:02 +0300

    Create money controller for managing excel's requests' endpoints

---
+++ CVapp/Controllers/MoneyController.cs
@@ -0,0 +1,32 @@
+﻿using CVapp.Domain.Models.Content;
+using CVapp.Infrastructure.Abstractions;
+using CVapp.Infrastructure.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVapp.API.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class MoneyController : ControllerBase
+    {
+        private readonly IMoneyService _moneyService;
+        public MoneyController(IMoneyService moneyService)
+        {
+            _moneyService = moneyService;
+        }
+
+        [HttpPost("saveExcel")]
+        public async Task<IActionResult> SaveTransactionsToExcel(List<MoneyDto> transactionsDto)
+        {
+            var result = await _moneyService.SaveTransactions(transactionsDto);
+            return Ok(result);
+        }
+
+        [HttpGet("generateExcel")]
+        public IActionResult GenerateWorksheet()
+        {
+            var result = _moneyService.GenerateAndDownloadExcel();
+            return Ok(result);
+        }
+    }
+}
