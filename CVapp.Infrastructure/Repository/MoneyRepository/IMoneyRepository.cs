e471e9f67148182e285e4e7e4007abdd25d1ae43
EvilPanda <joratractoristul@gmail.com>
2022-05-14 17:02:08 +0300

    Create money repository interface for the money repository methods

---
+++ CVapp.Infrastructure/Repository/MoneyRepository/IMoneyRepository.cs
@@ -0,0 +1,15 @@
+﻿using CVapp.Domain.Models.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.Repository.MoneyRepository
+{
+    public interface IMoneyRepository
+    {
+        public List<Money> GetTransactions();
+        public Task<List<Money>> SaveTransactions(List<Money> transactions);
+    }
+}
