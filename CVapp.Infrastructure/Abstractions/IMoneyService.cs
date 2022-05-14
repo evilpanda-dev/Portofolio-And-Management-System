eb3396dc68118f28ac82548a210801af1e2ebb10
EvilPanda <joratractoristul@gmail.com>
2022-05-14 17:02:38 +0300

    Create money's service interface for money service methods

---
+++ CVapp.Infrastructure/Abstractions/IMoneyService.cs
@@ -0,0 +1,17 @@
+﻿using CVapp.Domain.Models.Content;
+using CVapp.Infrastructure.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CVapp.Infrastructure.Abstractions
+{
+    public interface IMoneyService
+    {
+        public Task<List<MoneyDto>> SaveTransactions(List<MoneyDto> transactionsDto);
+        public string GenerateAndDownloadExcel();
+    }
+}
