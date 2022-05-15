09da3f41f6f250cbcb6ffc2a8c73d2378d3c9cf2
EvilPanda <joratractoristul@gmail.com>
2022-05-16 02:34:24 +0300

    Implement pagination,sorting,searching and filtering in TransactionTable component

--- src/components/Transactions/TransactionTable/TransactionTable.js
+++ src/components/Transactions/TransactionTable/TransactionTable.js
@@ -11,25 +11,45 @@ import Paper from "@material-ui/core/Paper";
 import SearchBar from "material-ui-search-bar";
 import TableSortLabel  from "@material-ui/core/TableSortLabel";
 import { getDatabaseData, searchForData, sortData } from "../../../api/displayDataApi";
-
+import { TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
 const TransactionTable = () => {
     const [dataFromDb, setDataFromDb] = useState([]);
     const [tableCurrentPage, setTableCurrentPage] = useState(0);
-    const [totalPages, setTotalPages] = useState(0);
+    // const [totalPages, setTotalPages] = useState(0);
     const [totalItems,setTotalItems] = useState(0);
-    const [rowsPerPage,setRowsPerPage] = useState(10);
+    const [rowsPerPage,setRowsPerPage] = useState(5);
     const [searched,setSearched] = useState("")
     const [order,setOrder] = useState("asc")
-    const [orderBy,setOrderBy] = useState("transactionDate")
-    
+    const [orderBy,setOrderBy] = useState("TransactionDate")
+    const [minPrice,setMinPrice] = useState(0)
+    const [maxPrice,setMaxPrice] = useState(0)
+    const [errors,setErrors] = useState({minimumPriceError: "", maximumPriceError:"",itemError:""})
+    const [isFiltering,setIsFiltering] = useState(false)
+    const [item,setItem] = useState("")
+
+    let queryParams = Object.assign({},
+      rowsPerPage === null ? null : {pageSize: rowsPerPage},
+      tableCurrentPage === null ? null : {pageNumber: tableCurrentPage},
+      minPrice === 0 ? null : {minPrice: minPrice},
+      maxPrice === 0 ? null : {maxPrice: maxPrice},
+      item === "" ? null : {item: item},
+      searched === "" ? null : {search: searched.toLowerCase()},
+      orderBy === "TransactionDate" ? null : {orderByProperty: orderBy},
+      order === "asc" ? null : {orderDirection: order}
+    )
+
     useEffect(()=>{
-      getDatabaseData(tableCurrentPage,rowsPerPage).then(data => {
+      getDatabaseData(queryParams).then(data => {
         setDataFromDb(data.transactions);
-        setTotalPages(data.pages);
+        // setTotalPages(data.pages);
         setTotalItems(data.totalItems);
       })
-    },[tableCurrentPage,rowsPerPage])
+      if(dataFromDb.length === 0){
+        setTableCurrentPage(0)
+      }
+    },[tableCurrentPage,rowsPerPage,isFiltering,orderBy])
 
     const useStyles = makeStyles({
         table: {
@@ -48,29 +68,54 @@ const TransactionTable = () => {
     }
 
     const requestSearch = (searchedVal) =>{
-      const filteredRows = searchForData(searchedVal).then(data => {
-        setDataFromDb(data);
-      })
+      setIsFiltering(true)
+      setSearched(searchedVal)
     }
 
     const cancelSearch = () =>{
       setSearched("");
-      requestSearch(searched);
+      setIsFiltering(false)
     }
 
     const requestSort = (element) => {
         const isAsc = order === "asc" ? "desc" : "asc";
-        sortData(element,isAsc).then(data => {
-          setDataFromDb(data);
           setOrder(isAsc);
           setOrderBy(element);
-        })
+          setIsFiltering(true)
         // setOrder(isAsc);
         // setOrderBy(element);
     }
 
+    const handleFormChange = (event) => {
+      const {target : {value}} = event
+        if(event.target.name === "minimumPrice"){
+          if(value < 0 ){
+            setErrors({minimumPriceError: "Minimum price must be greater than 0"})
+          }else if(value > 0){
+            setErrors({minimumPriceError: ""})
+            setMinPrice(value)
+          }
+        }
+        if(event.target.name === "maximumPrice"){
+          if(value < 0 ){
+            setErrors({maximumPriceError: "Maximum price must be greater than 0"})
+          } else if (value>0){
+            setErrors({maximumPriceError: ""})
+            setMaxPrice(value)
+          }
+        }
+        if(event.target.name === "item"){
+          if(typeof value === 'string' || value instanceof String){
+            setErrors({itemError: ""})
+            setItem(value)
+          } else { 
+            setErrors({itemError: "Item must be a text"})
+          }
+        }
+    }
+
     const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, totalItems.length - tableCurrentPage * rowsPerPage);
+    rowsPerPage - Math.min(rowsPerPage, totalItems - tableCurrentPage * rowsPerPage);
     
     if(dataFromDb.length > 0 ){
       dataFromDb.forEach(row => {
@@ -84,7 +129,7 @@ const TransactionTable = () => {
         <TableContainer component={Paper} style={{"maxHeight" : "600px"}}>
           <SearchBar
           value={searched}
-          onChange={(searchVal) => requestSearch(searchVal)}
+          onRequestSearch={(searchVal) => requestSearch(searchVal)}
           onCancelSearch={() => cancelSearch()}
         />
         <Table className={classes.table} aria-label="simple table">
@@ -94,7 +139,7 @@ const TransactionTable = () => {
               <TableCell><TableSortLabel align="right" onClick={()=>{requestSort("TransactionAccount")}} direction={order}>Transaction Account</TableSortLabel></TableCell>
               <TableCell><TableSortLabel align="right" onClick={()=>{requestSort("Category")}} direction={order}>Category</TableSortLabel></TableCell>
               <TableCell><TableSortLabel align="right" onClick={()=>{requestSort("Item")}} direction={order}>Item</TableSortLabel></TableCell>
-              <TableCell><TableSortLabel align="right" onClick={()=>{requestSort("Amount")}} direction={order}>Amount</TableSortLabel></TableCell>
+              <TableCell><TableSortLabel align="right" onClick={()=>{requestSort("Sum")}} direction={order}>Sum</TableSortLabel></TableCell>
               <TableCell><TableSortLabel align="right" onClick={()=>{requestSort("TransactionType")}} direction={order}>Transaction Type</TableSortLabel></TableCell>
               <TableCell><TableSortLabel align="right" onClick={()=>{requestSort("Currency")}} direction={order}>Currency</TableSortLabel></TableCell>
             </TableRow>
@@ -110,7 +155,7 @@ const TransactionTable = () => {
                   <TableCell align="right">{row.transactionAccount}</TableCell>
                   <TableCell align="right">{row.category}</TableCell>
                   <TableCell align="right">{row.item}</TableCell>
-                  <TableCell align="right">{row.amount}</TableCell>
+                  <TableCell align="right">{row.sum}</TableCell>
                   <TableCell align="right">{row.transactionType}</TableCell>
                   <TableCell align="right">{row.currency}</TableCell>
                 </TableRow>
@@ -135,6 +180,86 @@ const TransactionTable = () => {
           onPageChange={handleChangePage}
           onRowsPerPageChange={handleChangeRowsPerPage}
         />
+        <TextField
+          id="outlined-number"
+          label="Minimum Price"
+          type="number"
+          name="minimumPrice"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant = "outlined"
+          style={{
+            marginLeft : "10px",
+            marginBottom : "10px"
+          }}
+          value={minPrice}
+          onChange={handleFormChange}
+          error={Boolean(errors?.minimumPriceError)}
+          helperText={(errors?.minimumPriceError)}
+        />
+        <TextField
+          id="outlined-number"
+          label="Maximum Price"
+          type="number"
+          name="maximumPrice"
+          variant = "outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          style={{
+            marginLeft : "10px",
+          }}
+          required
+          value={maxPrice}
+          onChange={handleFormChange}
+          error={Boolean(errors?.maximumPriceError)}
+          helperText={(errors?.maximumPriceError)}
+        />
+         <TextField
+          id="outlined-number"
+          label="Item"
+          type="input"
+          name="item"
+          variant = "outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          style={{
+            marginLeft : "10px",
+          }}
+          required
+          value={item}
+          onChange={handleFormChange}
+          error={Boolean(errors?.itemError)}
+          helperText={(errors?.itemError)}
+        />
+        <Button 
+        variant="contained" 
+        color="primary" 
+        style={{
+          marginLeft : "10px",
+          marginTop : "10px"
+          }}
+          type="submit"
+        text="Submit"
+        onClick={()=>{
+          setIsFiltering(true)
+        }}> Submit</Button>
+        <Button 
+        variant="contained" 
+        color="secondary" 
+        style={{
+          marginLeft : "10px",
+          marginTop : "10px"}}
+        onClick={()=>{
+          setMinPrice(0);
+          setMaxPrice(0);
+          setItem("");
+          setErrors({minimumPriceError: "", maximumPriceError: "", itemError: ""})
+          setIsFiltering(false)
+        }}
+        >Reset</Button>
       </TableContainer>
     );
 }
