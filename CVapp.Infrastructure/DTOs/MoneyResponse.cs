daf79d29cc4cb165d61a3e7a8da4e24ee72a6008
EvilPanda <joratractoristul@gmail.com>
2022-05-15 03:36:57 +0300

    Create Money response dto for paginating

---
+++ CVapp.Infrastructure/DTOs/MoneyResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.DTOs
+{
+    public class MoneyResponse
+    {
+        public List<MoneyDto> Transactions { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalItems { get; set; }
+    }
+}
