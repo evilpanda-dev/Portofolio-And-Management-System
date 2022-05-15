bca51ee8aef6c35154da0478cd2209ffc39e7227
EvilPanda <joratractoristul@gmail.com>
2022-05-16 02:37:23 +0300

    Remove TotalPages property in MoneyResponse because it is uneccessary

--- CVapp.Infrastructure/DTOs/MoneyResponse.cs
+++ CVapp.Infrastructure/DTOs/MoneyResponse.cs
@@ -9,7 +9,7 @@ namespace CVapp.Infrastructure.DTOs
     public class MoneyResponse
     {
         public List<MoneyDto> Transactions { get; set; }
-        public int TotalPages { get; set; }
+        /*public int TotalPages { get; set; }*/
         public int CurrentPage { get; set; }
         public int TotalItems { get; set; }
     }
