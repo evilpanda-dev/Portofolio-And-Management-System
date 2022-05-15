da3170b9b9632e110799128856a111a1cd8dbabe
EvilPanda <joratractoristul@gmail.com>
2022-05-15 03:44:45 +0300

    Add function and label for sorting

--- src/components/Transactions/TransactionTable/TransactionTable.js
+++ src/components/Transactions/TransactionTable/TransactionTable.js
@@ -9,7 +9,8 @@ import TableRow from "@material-ui/core/TableRow";
 import TablePagination from "@material-ui/core/TablePagination";
 import Paper from "@material-ui/core/Paper";
 import SearchBar from "material-ui-search-bar";
-import { getDatabaseData, searchForData } from "../../../api/displayDataApi";
+import TableSortLabel  from "@material-ui/core/TableSortLabel";
+import { getDatabaseData, searchForData, sortData } from "../../../api/displayDataApi";
 
 
 const TransactionTable = () => {
@@ -19,7 +20,9 @@ const TransactionTable = () => {
     const [totalItems,setTotalItems] = useState(0);
     const [rowsPerPage,setRowsPerPage] = useState(10);
     const [searched,setSearched] = useState("")
-
+    const [order,setOrder] = useState("asc")
+    const [orderBy,setOrderBy] = useState("transactionDate")
+    
     useEffect(()=>{
       getDatabaseData(tableCurrentPage,rowsPerPage).then(data => {
         setDataFromDb(data.transactions);
@@ -55,6 +58,17 @@ const TransactionTable = () => {
       requestSearch(searched);
     }
 
+    const requestSort = (element) => {
+        const isAsc = order === "asc" ? "desc" : "asc";
+        sortData(element,isAsc).then(data => {
+          setDataFromDb(data);
+          setOrder(isAsc);
+          setOrderBy(element);
+        })
+        // setOrder(isAsc);
+        // setOrderBy(element);
+    }
+
     const emptyRows =
     rowsPerPage - Math.min(rowsPerPage, totalItems.length - tableCurrentPage * rowsPerPage);
     
@@ -76,17 +90,17 @@ const TransactionTable = () => {
         <Table className={classes.table} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Transaction Date</TableCell>
-              <TableCell align="right">Transaction Account</TableCell>
-              <TableCell align="right">Category</TableCell>
-              <TableCell align="right">Item</TableCell>
-              <TableCell align="right">Amount</TableCell>
-              <TableCell align="right">Transaction Type</TableCell>
-              <TableCell align="right">Currency</TableCell>
+              <TableCell><TableSortLabel align="right" onClick={()=>{requestSort("TransactionDate")}} direction={order}>Transaction Date</TableSortLabel></TableCell>
+              <TableCell><TableSortLabel align="right" onClick={()=>{requestSort("TransactionAccount")}} direction={order}>Transaction Account</TableSortLabel></TableCell>
+              <TableCell><TableSortLabel align="right" onClick={()=>{requestSort("Category")}} direction={order}>Category</TableSortLabel></TableCell>
+              <TableCell><TableSortLabel align="right" onClick={()=>{requestSort("Item")}} direction={order}>Item</TableSortLabel></TableCell>
+              <TableCell><TableSortLabel align="right" onClick={()=>{requestSort("Amount")}} direction={order}>Amount</TableSortLabel></TableCell>
+              <TableCell><TableSortLabel align="right" onClick={()=>{requestSort("TransactionType")}} direction={order}>Transaction Type</TableSortLabel></TableCell>
+              <TableCell><TableSortLabel align="right" onClick={()=>{requestSort("Currency")}} direction={order}>Currency</TableSortLabel></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {dataFromDb.length > 0 ? dataFromDb.map((row, index) => (
+            {dataFromDb.length > 0 ? dataFromDb.map((row) => (
               // .slice(tableCurrentPage * rowsPerPage, tableCurrentPage * rowsPerPage + rowsPerPage)
               // .map((row) => (
                 <TableRow key={row.id}>
