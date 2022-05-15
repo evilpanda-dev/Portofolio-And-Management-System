da958193dbc7cf5abaebc9736c8eac4be2671d86
EvilPanda <joratractoristul@gmail.com>
2022-05-16 02:37:42 +0300

    Create QueryParameters class for paginating parameters

---
+++ CVapp.Infrastructure/Query/QueryParameters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.Query
+{
+    public class QueryParameters
+    {
+        const int _maxSize = int.MaxValue;
+        private int _pageSize = 5;
+
+        public int PageNumber { get; set; }
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = value > _maxSize ? _maxSize : value;
+            }
+        }
+    }
+}
