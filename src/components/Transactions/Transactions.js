8aa64e3cc811ee024ea695fb68e2faff42be858b
EvilPanda <joratractoristul@gmail.com>
2022-05-16 02:59:59 +0300

    Add classnames for the buttonts in transactions component

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -4,7 +4,7 @@ import UploadFile from "./UploadFile.js"
 import DisplayData from "./DisplayData.js";
 import { getDatabaseData } from "../../api/displayDataApi.js";
 import TransactionTable from "./TransactionTable/TransactionTable";
-
+import './Transactions.css'
 const Transactions = () => {
     const [uploadedExcelData, setUploadedExcelData] = useState([]);
     const [dataForDb, setDataForDb] = useState([]);
@@ -26,13 +26,13 @@ const Transactions = () => {
         <UploadFile onUploadExcelFile={uploadedExcelDataHandler} />
       <DisplayData excelData={uploadedExcelData} setData={setDataForDb}/>
         </div>
-        <button onClick={()=>{
+        <button className="excelDownloadButton" onClick={()=>{
             generateAndDownloadExcel("generateExcel","GET")
         }}>Download Excel</button>
-        <button onClick={()=>{
+        <button className="saveToDbButton" onClick={()=>{
             saveToDatabase(dataForDb)
         }}>Save to database</button>
-        <button onClick={viewDatabaseTable}>View database table</button>
+        <button className="viewDbTableButton" onClick={viewDatabaseTable}>View database table</button>
         {isViewingData && 
         <TransactionTable />}
         </>
