22f3233dd5b6932bfc6812597ede2638919d4759
EvilPanda <joratractoristul@gmail.com>
2022-05-16 02:36:39 +0300

    Migrate the previous change to the database

---
+++ CVapp.Infrastructure/Data/Migrations/20220515175244_ChangeColumnNameInMoneyTableFromAmountToSum.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CVapp.Migrations
+{
+    public partial class ChangeColumnNameInMoneyTableFromAmountToSum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Amount",
+                table: "Money",
+                newName: "Sum");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Sum",
+                table: "Money",
+                newName: "Amount");
+        }
+    }
+}
