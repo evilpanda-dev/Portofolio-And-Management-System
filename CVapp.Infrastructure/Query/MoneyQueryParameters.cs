7f5e301b28daacfb5fe90a36654a996c0406c0c7
EvilPanda <joratractoristul@gmail.com>
2022-05-16 02:38:09 +0300

    Create moneyQuery parameters class for custom parameters for money controller

---
+++ CVapp.Infrastructure/Query/MoneyQueryParameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.Query
+{
+    public class MoneyQueryParameters : QueryParameters
+    {
+        public string? Item { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public string? Search { get; set; }
+        public string? OrderDirection { get; set; }
+        public string? OrderByProperty { get; set; }
+    }
+}
