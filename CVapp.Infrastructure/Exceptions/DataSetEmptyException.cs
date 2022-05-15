95d4f296b1d620991bbf2371bb1716326dcd734e
EvilPanda <joratractoristul@gmail.com>
2022-05-15 03:37:09 +0300

    Create DataSetEmpty exception

---
+++ CVapp.Infrastructure/Exceptions/DataSetEmptyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.Exceptions
+{
+    public class DataSetEmptyException : ApplicationException
+    {
+        public DataSetEmptyException(string message) : base(message)
+        {
+
+        }
+    }
+}
