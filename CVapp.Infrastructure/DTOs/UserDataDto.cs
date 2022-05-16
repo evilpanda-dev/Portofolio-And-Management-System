ee9ddfaf289f15060fcb3d9827aac11f5048229e
EvilPanda <joratractoristul@gmail.com>
2022-05-16 05:43:12 +0300

    Create user data dto for storing all the information about the users

---
+++ CVapp.Infrastructure/DTOs/UserDataDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVapp.Infrastructure.DTOs
+{
+    public class UserDataDto : UserDto
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public int? PhoneNumber { get; set; }
+        public string? AboutMe { get; set; }
+    }
+}
