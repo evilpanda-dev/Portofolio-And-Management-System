823f09b0a138a77e053dcd69d281200e47cdaf8f
EvilPanda <joratractoristul@gmail.com>
2022-05-16 23:05:35 +0300

    Create MorePieCharts component for storing two types of pie charts

---
+++ src/components/Charts/MorePieCharts.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import FilterButton from "../Button/FilterButton";
+import PieChart from "./PieChart";
+
+const MorePieCharts = () => {
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ]
+    
+      const allCategories = ["All", ...new Set(months.map((item) => item))];
+    
+      const [menuItems, setMenuItems] = useState(months);
+      const [categories] = useState(allCategories);
+    
+      const filterItem = (category) => {
+        if (category === "All") {
+          setMenuItems(months);
+          return;
+        }
+        const newItem = months.filter((item) => item === category);
+        setMenuItems(newItem);
+      };
+
+    return(
+        <div>
+          <h1 className="transactionsTitle">Expense pie chart with categories</h1>
+          <div >
+          <FilterButton categories={categories} filterItem={filterItem} />
+        </div>
+          {menuItems.map((month,index)=>{
+            return(
+              <div className="pieChart">
+              <PieChart month={month} transactionType="Expense" key={index}/>
+              </div>
+            )
+          })}
+          <h1 className="transactionsTitle">Income pie chart with categories</h1>
+          {/* <div >
+          <FilterButton categories={categories} filterItem={filterItem} />
+        </div> */}
+          {menuItems.map((month,index)=>{
+            return(
+              <div className="pieChart">
+              <PieChart month={month} transactionType="Income" key={index}/>
+              </div>
+            )
+          })}
+          </div>
+    )
+}
+
+export default MorePieCharts;
