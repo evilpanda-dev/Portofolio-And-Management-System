48ea23c28252c4d2da876c4379c22bafa1a225b4
EvilPanda <joratractoristul@gmail.com>
2022-05-16 19:58:36 +0300

    Add new endpoint in data controller for getting all the comments in database

--- CVapp/Controllers/DataController.cs
+++ CVapp/Controllers/DataController.cs
@@ -4,21 +4,29 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CVapp.API.Controllers
 {
-        [Route("api")]
-        [ApiController]
-        public class DataController : ControllerBase
+    [Route("api")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        private readonly IDataService _dataService;
+        public DataController(IDataService dataService)
         {
-            private readonly IDataService _dataService;
-            public DataController(IDataService dataService)
-            {
-                _dataService = dataService;
-            }
+            _dataService = dataService;
+        }
 
         [HttpGet("allUserData")]
         public IActionResult GetAllUserData([FromQuery] QueryParameters queryParameters)
         {
-        var result = _dataService.GetAllUsersAndTheirProfiles(queryParameters);
-        return Ok(result);
+            var result = _dataService.GetAllUsersAndTheirProfiles(queryParameters);
+            return Ok(result);
         }
-}
+
+        [HttpGet("allUserComments")]
+        public IActionResult GetAllUserComments([FromQuery] QueryParameters queryParameters)
+        {
+            var result = _dataService.GetAllComments(queryParameters);
+            return Ok(result);
+        }
+        
+    }
 }
