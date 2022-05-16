af4e349e78399f00a3fa8fbddc95c2fc0a59662d
EvilPanda <joratractoristul@gmail.com>
2022-05-16 23:04:28 +0300

    Add two cards with total number of users and comments on dashboard page

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -9,18 +9,33 @@ import UserDataTable from "../../components/UserDataTable/UserDataTable";
 import { useContext } from "react";
 import { AlertContext } from "../../providers/AlertProvider";
 import CommentsTable from "../../components/CommentsTable/CommentsTable";
+import { UserCountContext } from "../../providers/UserCountProvider";
+import { CommentCountContext } from "../../providers/CommentCountProvider";
 
 const Dashboard = props =>{
     const {
         imageSrc
      } = props
      const {alert} = useContext(AlertContext)
+     const {userCount} = useContext(UserCountContext)
+     const {commentsCount} = useContext(CommentCountContext)
     return(
         <>
         <Panel/>
         <Header imageSrc={imageSrc}/>
         <section className="dashboardPage">
             {alert.appAlerts}
+                <h1 className="transactionsTitle">Welcome to your dashboard</h1>
+            <div className="cardBox">
+                <div className="card">
+                <h2 className="numbers">{userCount.totalUsers}</h2>
+                <h4 className="cardName"> Total users</h4>
+                </div>
+                <div className="card">
+                <h2 className="numbers">{commentsCount.totalComments}</h2>
+                <h4 className="cardName"> Total comments</h4>
+                </div>
+            </div>
        <UserDataTable/>
 <CommentsTable/>
             <Transactions/>
