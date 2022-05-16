6096bbe3b98019b51b1799c0a99fa98169e4ae1a
EvilPanda <joratractoristul@gmail.com>
2022-05-16 05:35:54 +0300

    Add alerts on the dashboard page

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -6,16 +6,21 @@ import WakatimeData from '../../utils/wakaTimeData.js'
 import Charts from "../../components/Charts/Charts";
 import Transactions from "../../components/Transactions/Transactions";
 import UserDataTable from "../../components/UserDataTable/UserDataTable";
+import { useContext } from "react";
+import { AlertContext } from "../../providers/AlertProvider";
 
 const Dashboard = props =>{
     const {
         imageSrc
      } = props
+     const {alert} = useContext(AlertContext)
     return(
         <>
+        
         <Panel/>
         <Header imageSrc={imageSrc}/>
         <section className="dashboardPage">
+            {alert.appAlerts}
             <Transactions/>
        <UserDataTable/>
        <Charts data={WakatimeData}/>
