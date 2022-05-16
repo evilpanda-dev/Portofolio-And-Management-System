7442866f676cc6777797734dceaa965d1df1391d
EvilPanda <joratractoristul@gmail.com>
2022-05-17 00:30:05 +0300

    Create noPemission page to be displayed on unauthorized access

---
+++ src/pages/NoPermission/NoPermission.js
@@ -0,0 +1,24 @@
+import './NoPermission.css'
+
+const NoPermission = () => {
+    return(
+        <div className="noPermissionTextWrapper">
+    <div className="noPermissionTitle" data-content="404">
+        403 - ACCESS DENIED
+    </div>
+
+    <div className="noPermissionSubtitle">
+        Oops, You don't have permission to access this page.
+    </div>
+    <div className="isi">
+         A web server may return a 403 Forbidden HTTP status code in response to a request from a client for a web page or resource to indicate that the server can be reached and understood the request, but refuses to take any further action. Status code 403 responses are the result of the web server being configured to deny access, for some reason, to the requested resource by the client.
+         </div>
+
+    <div className="noPermissionButtons">
+        <a className="noPermissionButton" href="/">Go to homepage</a>
+    </div>
+</div>
+    )
+}
+
+export default NoPermission
