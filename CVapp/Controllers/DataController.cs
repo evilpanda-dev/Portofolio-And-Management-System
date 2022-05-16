d8ccd8dfe82809fed232cb05ac7ab1fc0d3e5aee
EvilPanda <joratractoristul@gmail.com>
2022-05-16 05:43:56 +0300

    Create data controller and add allUserData endpoint for retrieving data about users

---
+++ CVapp/Controllers/DataController.cs
@@ -0,0 +1,24 @@
+﻿using CVapp.Infrastructure.Abstractions;
+using CVapp.Infrastructure.Query;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVapp.API.Controllers
+{
+        [Route("api")]
+        [ApiController]
+        public class DataController : ControllerBase
+        {
+            private readonly IDataService _dataService;
+            public DataController(IDataService dataService)
+            {
+                _dataService = dataService;
+            }
+
+        [HttpGet("allUserData")]
+        public IActionResult GetAllUserData([FromQuery] QueryParameters queryParameters)
+        {
+        var result = _dataService.GetAllUsersAndTheirProfiles(queryParameters);
+        return Ok(result);
+        }
+}
+}
