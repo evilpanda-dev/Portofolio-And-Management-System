152d8a1f5189d40d1d4c5cf4a1dabbb00f2668b2
EvilPanda <joratractoristul@gmail.com>
2022-05-17 00:43:00 +0300

    Create not found page for redirecting lost users to

---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
+
+const NotFound = () => (
+    <>
+<section className="error-container">
+  <span className="four"><span className="screen-reader-text">4</span></span>
+  <span className="zero"><span className="screen-reader-text">0</span></span>
+  <span className="four"><span className="screen-reader-text">4</span></span>
+</section>
+<div className="link-container">
+<Link className="more-link" to="/">Go Home</Link>
+</div>
+</>
+);
+
+export default NotFound;
